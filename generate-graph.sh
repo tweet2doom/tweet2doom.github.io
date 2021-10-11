37a57238fe3722b8e3efbf6b62e53069ddcfb895
Tweet2Doom <tweet2doom@gmail.com>
2021-10-11 08:51:23 +0300

    Open twitter on node click

--- generate-graph.sh
+++ generate-graph.sh
@@ -11,10 +11,13 @@ for id in `ls data/nodes` ; do
     if [ "$id" = "root" ] ; then continue ; fi
     depth=$(cat data/nodes/$id/depth)
     depth=$((2*$depth + 0))
-    # does not work on Safari iOS
-    echo "nodes.push({ id: $id, label: \"\", level: $depth, group: \"node\", image: \"images/logo.png\" });" >> $js
 
-    #echo "nodes.push({ id: $id, label: \"\", level: $depth, group: \"node\" });" >> $js
+    if [ "$id" = "1444355917160534024" ] ; then
+        # root node
+        echo "nodes.push({ id: '$id', label: \"ROOT\", level: $depth, group: \"node\", image: \"images/logo.png\" });" >> $js
+    else
+        echo "nodes.push({ id: '$id', label: \"\", level: $depth, group: \"node\", image: \"images/logo.png\" });" >> $js
+    fi
 done
 
 for id in `ls data/processed` ; do
@@ -23,16 +26,16 @@ for id in `ls data/processed` ; do
     username=$(cat data/processed/$id/username)
     #cmd=$(cat data/processed/$id/cmd.txt | tr -d '\n')
     #echo "nodes.push({ id: $id, label: \"$username\", title: \"$cmd\", level: $depth, group: \"command\", font: { face: \"Liberation Mono\", size: 12 }});" >> $js
-    echo "nodes.push({ id: $id, label: \"$username\", level: $depth, group: \"command\", font: { face: \"Liberation Mono\", size: 12 } });" >> $js
+    echo "nodes.push({ id: '$id', label: \"$username\", level: $depth, group: \"command\", font: { face: \"Liberation Mono\", size: 12 }, chosen: false });" >> $js
 done
 
 echo "" >> $js
 
 for id in `ls data/processed` ; do
-    parent_id=$(cat data/processed/$id/parent_id)
-    child_id=$(cat data/processed/$id/child_id)
-    echo "edges.push({ from: $id, to: $parent_id });" >> $js
-    echo "edges.push({ from: $child_id, to: $id });" >> $js
+    parent_id="$(cat data/processed/$id/parent_id)"
+    child_id="$(cat data/processed/$id/child_id)"
+    echo "edges.push({ from: '$id', to: '$parent_id' });" >> $js
+    echo "edges.push({ from: '$child_id', to: '$id' });" >> $js
 done
 
 echo "}" >> $js
