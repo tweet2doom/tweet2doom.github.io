c7f86e8116f15d2c806909fbc332ba67a34c21f8
Tweet2Doom <tweet2doom@gmail.com>
2021-10-16 17:30:48 +0000

    New states added [Sat 16 Oct 2021 05:30:48 PM UTC]

--- js/graph.js
+++ js/graph.js
@@ -1394,6 +1394,7 @@ nodes.push({ id: '1449426006692016131', label: "", level: 114, group: "node", im
 nodes.push({ id: '1449426431327576073', label: "", level: 34, group: "node", image: "images/logo.png" });
 nodes.push({ id: '1449426648365944839', label: "", level: 40, group: "node", image: "images/logo.png" });
 nodes.push({ id: '1449427291281428484', label: "", level: 2, group: "node", image: "images/logo.png" });
+nodes.push({ id: '1449427489231605775', label: "", level: 42, group: "node", image: "images/logo.png" });
 nodes.push({ id: '1444358348955164684', label: "tweet2doom_info", level: 1, group: "command", font: { face: "Liberation Mono", size: 12 }, chosen: false });
 nodes.push({ id: '1444359046136479752', label: "ggerganov", level: 1, group: "command", font: { face: "Liberation Mono", size: 12 }, chosen: false });
 nodes.push({ id: '1444359352073216003', label: "ggerganov", level: 3, group: "command", font: { face: "Liberation Mono", size: 12 }, chosen: false });
@@ -2787,6 +2788,7 @@ nodes.push({ id: '1449425691678855174', label: "ggerganov", level: 113, group: "
 nodes.push({ id: '1449426319691952135', label: "plague_spreader", level: 33, group: "command", font: { face: "Liberation Mono", size: 12 }, chosen: false });
 nodes.push({ id: '1449426509182038024', label: "migero_usausagi", level: 39, group: "command", font: { face: "Liberation Mono", size: 12 }, chosen: false });
 nodes.push({ id: '1449427206946557954', label: "news_2006", level: 1, group: "command", font: { face: "Liberation Mono", size: 12 }, chosen: false });
+nodes.push({ id: '1449427364182691845', label: "migero_usausagi", level: 41, group: "command", font: { face: "Liberation Mono", size: 12 }, chosen: false });
 
 edges.push({ from: '1444358348955164684', to: '1444355917160534024' });
 edges.push({ from: '1444358418337239043', to: '1444358348955164684' });
@@ -5574,4 +5576,6 @@ edges.push({ from: '1449426509182038024', to: '1449425858549198853' });
 edges.push({ from: '1449426648365944839', to: '1449426509182038024' });
 edges.push({ from: '1449427206946557954', to: '1444355917160534024' });
 edges.push({ from: '1449427291281428484', to: '1449427206946557954' });
+edges.push({ from: '1449427364182691845', to: '1449426648365944839' });
+edges.push({ from: '1449427489231605775', to: '1449427364182691845' });
 }
