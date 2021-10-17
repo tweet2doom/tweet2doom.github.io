81f2e187e3482118b3060a7d6752199c57f3900d
Tweet2Doom <tweet2doom@gmail.com>
2021-10-17 08:57:44 +0000

    New states added [Sun 17 Oct 2021 08:57:43 AM UTC]

--- js/graph.js
+++ js/graph.js
@@ -1614,6 +1614,7 @@ nodes.push({ id: '1449655420704935938', label: "", level: 40, group: "node", ima
 nodes.push({ id: '1449655887866564608', label: "", level: 42, group: "node", image: "images/logo.png" });
 nodes.push({ id: '1449656449123094529', label: "", level: 2, group: "node", image: "images/logo.png" });
 nodes.push({ id: '1449657459195396096', label: "", level: 2, group: "node", image: "images/logo.png" });
+nodes.push({ id: '1449660743260020737', label: "", level: 44, group: "node", image: "images/logo.png" });
 nodes.push({ id: '1444358348955164684', label: "tweet2doom_info", level: 1, group: "command", font: { face: "Liberation Mono", size: 12 }, chosen: false });
 nodes.push({ id: '1444359046136479752', label: "ggerganov", level: 1, group: "command", font: { face: "Liberation Mono", size: 12 }, chosen: false });
 nodes.push({ id: '1444359352073216003', label: "ggerganov", level: 3, group: "command", font: { face: "Liberation Mono", size: 12 }, chosen: false });
@@ -3226,7 +3227,8 @@ nodes.push({ id: '1449654680729104384', label: "GammarikD", level: 39, group: "c
 nodes.push({ id: '1449655307966337027', label: "GammarikD", level: 39, group: "command", font: { face: "Liberation Mono", size: 12 }, chosen: false });
 nodes.push({ id: '1449655769155080195', label: "GammarikD", level: 41, group: "command", font: { face: "Liberation Mono", size: 12 }, chosen: false });
 nodes.push({ id: '1449656371570429952', label: "NOHATAYT", level: 1, group: "command", font: { face: "Liberation Mono", size: 12 }, chosen: false });
-nodes.push({ id: '1449657349044465664', label: "", level: -2, group: "command", font: { face: "Liberation Mono", size: 12 }, chosen: false });
+nodes.push({ id: '1449657349044465664', label: "taketakeninngen", level: 1, group: "command", font: { face: "Liberation Mono", size: 12 }, chosen: false });
+nodes.push({ id: '1449660606806646784', label: "GammarikD", level: 43, group: "command", font: { face: "Liberation Mono", size: 12 }, chosen: false });
 
 edges.push({ from: '1444358348955164684', to: '1444355917160534024' });
 edges.push({ from: '1444358418337239043', to: '1444358348955164684' });
@@ -6452,6 +6454,8 @@ edges.push({ from: '1449655769155080195', to: '1449655420704935938' });
 edges.push({ from: '1449655887866564608', to: '1449655769155080195' });
 edges.push({ from: '1449656371570429952', to: '1444355917160534024' });
 edges.push({ from: '1449656449123094529', to: '1449656371570429952' });
-edges.push({ from: '1449657349044465664', to: '' });
+edges.push({ from: '1449657349044465664', to: '1444355917160534024' });
 edges.push({ from: '1449657459195396096', to: '1449657349044465664' });
+edges.push({ from: '1449660606806646784', to: '1449655887866564608' });
+edges.push({ from: '1449660743260020737', to: '1449660606806646784' });
 }
