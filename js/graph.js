832cffcf0df9ac5d5fd7290d40165de8e86fac08
Tweet2Doom <tweet2doom@gmail.com>
2021-10-18 02:14:40 +0000

    New states added [Mon 18 Oct 2021 02:14:40 AM UTC]

--- js/graph.js
+++ js/graph.js
@@ -2274,6 +2274,7 @@ nodes.push({ id: '1449920929598001158', label: "", level: 156, group: "node", im
 nodes.push({ id: '1449921130064646147', label: "", level: 6, group: "node", image: "images/logo.png" });
 nodes.push({ id: '1449921281151864832', label: "", level: 2, group: "node", image: "images/logo.png" });
 nodes.push({ id: '1449921430175498247', label: "", level: 158, group: "node", image: "images/logo.png" });
+nodes.push({ id: '1449921640452837381', label: "", level: 8, group: "node", image: "images/logo.png" });
 nodes.push({ id: '1444358348955164684', label: "tweet2doom_info", level: 1, group: "command", font: { face: "Liberation Mono", size: 12 }, chosen: false });
 nodes.push({ id: '1444359046136479752', label: "ggerganov", level: 1, group: "command", font: { face: "Liberation Mono", size: 12 }, chosen: false });
 nodes.push({ id: '1444359352073216003', label: "ggerganov", level: 3, group: "command", font: { face: "Liberation Mono", size: 12 }, chosen: false });
@@ -4546,7 +4547,8 @@ nodes.push({ id: '1449920626903289860', label: "Xenthio", level: 3, group: "comm
 nodes.push({ id: '1449920642971836421', label: "ManWithNoGame45", level: 155, group: "command", font: { face: "Liberation Mono", size: 12 }, chosen: false });
 nodes.push({ id: '1449921024414285824', label: "Xenthio", level: 5, group: "command", font: { face: "Liberation Mono", size: 12 }, chosen: false });
 nodes.push({ id: '1449921054126784512', label: "Gaming68472198", level: 1, group: "command", font: { face: "Liberation Mono", size: 12 }, chosen: false });
-nodes.push({ id: '1449921125400645636', label: "", level: -2, group: "command", font: { face: "Liberation Mono", size: 12 }, chosen: false });
+nodes.push({ id: '1449921125400645636', label: "ManWithNoGame45", level: 157, group: "command", font: { face: "Liberation Mono", size: 12 }, chosen: false });
+nodes.push({ id: '1449921515693039621', label: "Xenthio", level: 7, group: "command", font: { face: "Liberation Mono", size: 12 }, chosen: false });
 
 edges.push({ from: '1444358348955164684', to: '1444355917160534024' });
 edges.push({ from: '1444358418337239043', to: '1444358348955164684' });
@@ -9092,6 +9094,8 @@ edges.push({ from: '1449921024414285824', to: '1449920783824953346' });
 edges.push({ from: '1449921130064646147', to: '1449921024414285824' });
 edges.push({ from: '1449921054126784512', to: '1444355917160534024' });
 edges.push({ from: '1449921281151864832', to: '1449921054126784512' });
-edges.push({ from: '1449921125400645636', to: '' });
+edges.push({ from: '1449921125400645636', to: '1449920929598001158' });
 edges.push({ from: '1449921430175498247', to: '1449921125400645636' });
+edges.push({ from: '1449921515693039621', to: '1449921130064646147' });
+edges.push({ from: '1449921640452837381', to: '1449921515693039621' });
 }
