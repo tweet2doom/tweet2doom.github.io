92654608caf19341057f5fc6a0469a6a5974d2b9
Tweet2Doom <tweet2doom@gmail.com>
2021-10-18 13:20:52 +0000

    New states added [Mon 18 Oct 2021 01:20:51 PM UTC]

--- js/graph.js
+++ js/graph.js
@@ -2832,6 +2832,8 @@ nodes.push({ id: '1450088531502714882', label: "", level: 2, group: "node", imag
 nodes.push({ id: '1450088692446601230', label: "", level: 2, group: "node", image: "images/logo.png" });
 nodes.push({ id: '1450088841050738689', label: "", level: 2, group: "node", image: "images/logo.png" });
 nodes.push({ id: '1450088991131373573', label: "", level: 2, group: "node", image: "images/logo.png" });
+nodes.push({ id: '1450089148380065795', label: "", level: 154, group: "node", image: "images/logo.png" });
+nodes.push({ id: '1450089293867802624', label: "", level: 2, group: "node", image: "images/logo.png" });
 nodes.push({ id: '1444358348955164684', label: "tweet2doom_info", level: 1, group: "command", font: { face: "Liberation Mono", size: 12 }, chosen: false });
 nodes.push({ id: '1444359046136479752', label: "ggerganov", level: 1, group: "command", font: { face: "Liberation Mono", size: 12 }, chosen: false });
 nodes.push({ id: '1444359352073216003', label: "ggerganov", level: 3, group: "command", font: { face: "Liberation Mono", size: 12 }, chosen: false });
@@ -5663,6 +5665,8 @@ nodes.push({ id: '1450088084113170432', label: "Aideshomemade", level: 1, group:
 nodes.push({ id: '1450088319287713798', label: "Angelduster44", level: 1, group: "command", font: { face: "Liberation Mono", size: 12 }, chosen: false });
 nodes.push({ id: '1450088381015265287', label: "GerardoRG2", level: 1, group: "command", font: { face: "Liberation Mono", size: 12 }, chosen: false });
 nodes.push({ id: '1450088383238348800', label: "JimmyButlerisTG", level: 1, group: "command", font: { face: "Liberation Mono", size: 12 }, chosen: false });
+nodes.push({ id: '1450088536674381826', label: "migero_usausagi", level: 153, group: "command", font: { face: "Liberation Mono", size: 12 }, chosen: false });
+nodes.push({ id: '1450088575572250631', label: "JesusArtz_", level: 1, group: "command", font: { face: "Liberation Mono", size: 12 }, chosen: false });
 
 edges.push({ from: '1444358348955164684', to: '1444355917160534024' });
 edges.push({ from: '1444358418337239043', to: '1444358348955164684' });
@@ -11326,4 +11330,8 @@ edges.push({ from: '1450088381015265287', to: '1444355917160534024' });
 edges.push({ from: '1450088841050738689', to: '1450088381015265287' });
 edges.push({ from: '1450088383238348800', to: '1444355917160534024' });
 edges.push({ from: '1450088991131373573', to: '1450088383238348800' });
+edges.push({ from: '1450088536674381826', to: '1450087326286233602' });
+edges.push({ from: '1450089148380065795', to: '1450088536674381826' });
+edges.push({ from: '1450088575572250631', to: '1444355917160534024' });
+edges.push({ from: '1450089293867802624', to: '1450088575572250631' });
 }
