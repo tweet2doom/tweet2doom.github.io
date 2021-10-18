4f781f0a0e76c86b4c377e7bb150d507e2ad5681
Tweet2Doom <tweet2doom@gmail.com>
2021-10-18 10:58:42 +0000

    New states added [Mon 18 Oct 2021 10:58:42 AM UTC]

--- js/graph.js
+++ js/graph.js
@@ -2727,6 +2727,8 @@ nodes.push({ id: '1450052695910453254', label: "", level: 314, group: "node", im
 nodes.push({ id: '1450052855533080577', label: "", level: 116, group: "node", image: "images/logo.png" });
 nodes.push({ id: '1450052997313093637', label: "", level: 2, group: "node", image: "images/logo.png" });
 nodes.push({ id: '1450053150875037696', label: "", level: 2, group: "node", image: "images/logo.png" });
+nodes.push({ id: '1450053373395353603', label: "", level: 118, group: "node", image: "images/logo.png" });
+nodes.push({ id: '1450053522909765641', label: "", level: 2, group: "node", image: "images/logo.png" });
 nodes.push({ id: '1444358348955164684', label: "tweet2doom_info", level: 1, group: "command", font: { face: "Liberation Mono", size: 12 }, chosen: false });
 nodes.push({ id: '1444359046136479752', label: "ggerganov", level: 1, group: "command", font: { face: "Liberation Mono", size: 12 }, chosen: false });
 nodes.push({ id: '1444359352073216003', label: "ggerganov", level: 3, group: "command", font: { face: "Liberation Mono", size: 12 }, chosen: false });
@@ -5453,6 +5455,8 @@ nodes.push({ id: '1450052344373288961', label: "GammarikD", level: 313, group: "
 nodes.push({ id: '1450052441463042052', label: "migero_usausagi", level: 115, group: "command", font: { face: "Liberation Mono", size: 12 }, chosen: false });
 nodes.push({ id: '1450052776562733057', label: "TheCalWeathers", level: 1, group: "command", font: { face: "Liberation Mono", size: 12 }, chosen: false });
 nodes.push({ id: '1450052835048001536', label: "KirikoHz", level: 1, group: "command", font: { face: "Liberation Mono", size: 12 }, chosen: false });
+nodes.push({ id: '1450053234463289347', label: "migero_usausagi", level: 117, group: "command", font: { face: "Liberation Mono", size: 12 }, chosen: false });
+nodes.push({ id: '1450053356869750786', label: "KirikoHz", level: 1, group: "command", font: { face: "Liberation Mono", size: 12 }, chosen: false });
 
 edges.push({ from: '1444358348955164684', to: '1444355917160534024' });
 edges.push({ from: '1444358418337239043', to: '1444358348955164684' });
@@ -10906,4 +10910,8 @@ edges.push({ from: '1450052776562733057', to: '1444355917160534024' });
 edges.push({ from: '1450052997313093637', to: '1450052776562733057' });
 edges.push({ from: '1450052835048001536', to: '1444355917160534024' });
 edges.push({ from: '1450053150875037696', to: '1450052835048001536' });
+edges.push({ from: '1450053234463289347', to: '1450052855533080577' });
+edges.push({ from: '1450053373395353603', to: '1450053234463289347' });
+edges.push({ from: '1450053356869750786', to: '1444355917160534024' });
+edges.push({ from: '1450053522909765641', to: '1450053356869750786' });
 }
