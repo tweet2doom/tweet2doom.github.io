1d19c1904e67fa7f66249e1e27a5cebeb4f801fd
Tweet2Doom <tweet2doom@gmail.com>
2021-10-18 02:38:05 +0000

    New states added [Mon 18 Oct 2021 02:38:05 AM UTC]

--- js/graph.js
+++ js/graph.js
@@ -2307,6 +2307,8 @@ nodes.push({ id: '1449926845953101826', label: "", level: 2, group: "node", imag
 nodes.push({ id: '1449926995522039812', label: "", level: 2, group: "node", image: "images/logo.png" });
 nodes.push({ id: '1449927149167783936', label: "", level: 2, group: "node", image: "images/logo.png" });
 nodes.push({ id: '1449927304986116096', label: "", level: 180, group: "node", image: "images/logo.png" });
+nodes.push({ id: '1449927452176814083', label: "", level: 2, group: "node", image: "images/logo.png" });
+nodes.push({ id: '1449927602798579716', label: "", level: 182, group: "node", image: "images/logo.png" });
 nodes.push({ id: '1444358348955164684', label: "tweet2doom_info", level: 1, group: "command", font: { face: "Liberation Mono", size: 12 }, chosen: false });
 nodes.push({ id: '1444359046136479752', label: "ggerganov", level: 1, group: "command", font: { face: "Liberation Mono", size: 12 }, chosen: false });
 nodes.push({ id: '1444359352073216003', label: "ggerganov", level: 3, group: "command", font: { face: "Liberation Mono", size: 12 }, chosen: false });
@@ -4613,6 +4615,8 @@ nodes.push({ id: '1449926543766085636', label: "zach_cortes", level: 1, group: "
 nodes.push({ id: '1449926558475505664', label: "ZachRya71490433", level: 1, group: "command", font: { face: "Liberation Mono", size: 12 }, chosen: false });
 nodes.push({ id: '1449926588959793159', label: "Metalblue19911", level: 1, group: "command", font: { face: "Liberation Mono", size: 12 }, chosen: false });
 nodes.push({ id: '1449926811555704833', label: "ManWithNoGame45", level: 179, group: "command", font: { face: "Liberation Mono", size: 12 }, chosen: false });
+nodes.push({ id: '1449927202506674176', label: "Veno202", level: 1, group: "command", font: { face: "Liberation Mono", size: 12 }, chosen: false });
+nodes.push({ id: '1449927463946145793', label: "ManWithNoGame45", level: 181, group: "command", font: { face: "Liberation Mono", size: 12 }, chosen: false });
 
 edges.push({ from: '1444358348955164684', to: '1444355917160534024' });
 edges.push({ from: '1444358418337239043', to: '1444358348955164684' });
@@ -9226,4 +9230,8 @@ edges.push({ from: '1449926588959793159', to: '1444355917160534024' });
 edges.push({ from: '1449927149167783936', to: '1449926588959793159' });
 edges.push({ from: '1449926811555704833', to: '1449926546639237123' });
 edges.push({ from: '1449927304986116096', to: '1449926811555704833' });
+edges.push({ from: '1449927202506674176', to: '1444355917160534024' });
+edges.push({ from: '1449927452176814083', to: '1449927202506674176' });
+edges.push({ from: '1449927463946145793', to: '1449927304986116096' });
+edges.push({ from: '1449927602798579716', to: '1449927463946145793' });
 }
