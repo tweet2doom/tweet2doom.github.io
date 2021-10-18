d1765c6cf3e1f9a743e9ee67322768254d83b37d
Tweet2Doom <tweet2doom@gmail.com>
2021-10-18 02:27:45 +0000

    New states added [Mon 18 Oct 2021 02:27:45 AM UTC]

--- js/graph.js
+++ js/graph.js
@@ -2292,6 +2292,8 @@ nodes.push({ id: '1449924205936533506', label: "", level: 170, group: "node", im
 nodes.push({ id: '1449924354737856514', label: "", level: 30, group: "node", image: "images/logo.png" });
 nodes.push({ id: '1449924504550051847', label: "", level: 20, group: "node", image: "images/logo.png" });
 nodes.push({ id: '1449924661848989699', label: "", level: 172, group: "node", image: "images/logo.png" });
+nodes.push({ id: '1449924816086130688', label: "", level: 2, group: "node", image: "images/logo.png" });
+nodes.push({ id: '1449924958705041416', label: "", level: 2, group: "node", image: "images/logo.png" });
 nodes.push({ id: '1444358348955164684', label: "tweet2doom_info", level: 1, group: "command", font: { face: "Liberation Mono", size: 12 }, chosen: false });
 nodes.push({ id: '1444359046136479752', label: "ggerganov", level: 1, group: "command", font: { face: "Liberation Mono", size: 12 }, chosen: false });
 nodes.push({ id: '1444359352073216003', label: "ggerganov", level: 3, group: "command", font: { face: "Liberation Mono", size: 12 }, chosen: false });
@@ -4583,6 +4585,8 @@ nodes.push({ id: '1449923775827484674', label: "ManWithNoGame45", level: 169, gr
 nodes.push({ id: '1449924069512544258', label: "Spawncho2", level: 29, group: "command", font: { face: "Liberation Mono", size: 12 }, chosen: false });
 nodes.push({ id: '1449924352804220934', label: "Xenthio", level: 19, group: "command", font: { face: "Liberation Mono", size: 12 }, chosen: false });
 nodes.push({ id: '1449924369304756225', label: "ManWithNoGame45", level: 171, group: "command", font: { face: "Liberation Mono", size: 12 }, chosen: false });
+nodes.push({ id: '1449924486212595713', label: "PietroPrata", level: 1, group: "command", font: { face: "Liberation Mono", size: 12 }, chosen: false });
+nodes.push({ id: '1449924581058400259', label: "Spideraspect191", level: 1, group: "command", font: { face: "Liberation Mono", size: 12 }, chosen: false });
 
 edges.push({ from: '1444358348955164684', to: '1444355917160534024' });
 edges.push({ from: '1444358418337239043', to: '1444358348955164684' });
@@ -9166,4 +9170,8 @@ edges.push({ from: '1449924352804220934', to: '1449924053502996488' });
 edges.push({ from: '1449924504550051847', to: '1449924352804220934' });
 edges.push({ from: '1449924369304756225', to: '1449924205936533506' });
 edges.push({ from: '1449924661848989699', to: '1449924369304756225' });
+edges.push({ from: '1449924486212595713', to: '1444355917160534024' });
+edges.push({ from: '1449924816086130688', to: '1449924486212595713' });
+edges.push({ from: '1449924581058400259', to: '1444355917160534024' });
+edges.push({ from: '1449924958705041416', to: '1449924581058400259' });
 }
