ff71470f01db65b355a1df146aae3fc846c5d69b
Tweet2Doom <tweet2doom@gmail.com>
2021-10-18 06:17:02 +0000

    New states added [Mon 18 Oct 2021 06:17:01 AM UTC]

--- js/graph.js
+++ js/graph.js
@@ -2477,6 +2477,8 @@ nodes.push({ id: '1449981887590584323', label: "", level: 2, group: "node", imag
 nodes.push({ id: '1449982037797097473', label: "", level: 2, group: "node", image: "images/logo.png" });
 nodes.push({ id: '1449982185973354498', label: "", level: 2, group: "node", image: "images/logo.png" });
 nodes.push({ id: '1449982342324531209', label: "", level: 2, group: "node", image: "images/logo.png" });
+nodes.push({ id: '1449982490567979009', label: "", level: 2, group: "node", image: "images/logo.png" });
+nodes.push({ id: '1449982638706593792', label: "", level: 2, group: "node", image: "images/logo.png" });
 nodes.push({ id: '1444358348955164684', label: "tweet2doom_info", level: 1, group: "command", font: { face: "Liberation Mono", size: 12 }, chosen: false });
 nodes.push({ id: '1444359046136479752', label: "ggerganov", level: 1, group: "command", font: { face: "Liberation Mono", size: 12 }, chosen: false });
 nodes.push({ id: '1444359352073216003', label: "ggerganov", level: 3, group: "command", font: { face: "Liberation Mono", size: 12 }, chosen: false });
@@ -4953,6 +4955,8 @@ nodes.push({ id: '1449981510849753093', label: "Heart87909302", level: 1, group:
 nodes.push({ id: '1449981541522698245', label: "HoodyCloud", level: 1, group: "command", font: { face: "Liberation Mono", size: 12 }, chosen: false });
 nodes.push({ id: '1449981741922353157', label: "WorkJa2546", level: 1, group: "command", font: { face: "Liberation Mono", size: 12 }, chosen: false });
 nodes.push({ id: '1449981934830972936', label: "N6XZsisaQcR45Vn", level: 1, group: "command", font: { face: "Liberation Mono", size: 12 }, chosen: false });
+nodes.push({ id: '1449981996655005700', label: "random5546", level: 1, group: "command", font: { face: "Liberation Mono", size: 12 }, chosen: false });
+nodes.push({ id: '1449982024773562372', label: "WaTaHaNaKiJiAMA", level: 1, group: "command", font: { face: "Liberation Mono", size: 12 }, chosen: false });
 
 edges.push({ from: '1444358348955164684', to: '1444355917160534024' });
 edges.push({ from: '1444358418337239043', to: '1444358348955164684' });
@@ -9906,4 +9910,8 @@ edges.push({ from: '1449981741922353157', to: '1444355917160534024' });
 edges.push({ from: '1449982185973354498', to: '1449981741922353157' });
 edges.push({ from: '1449981934830972936', to: '1444355917160534024' });
 edges.push({ from: '1449982342324531209', to: '1449981934830972936' });
+edges.push({ from: '1449981996655005700', to: '1444355917160534024' });
+edges.push({ from: '1449982490567979009', to: '1449981996655005700' });
+edges.push({ from: '1449982024773562372', to: '1444355917160534024' });
+edges.push({ from: '1449982638706593792', to: '1449982024773562372' });
 }
