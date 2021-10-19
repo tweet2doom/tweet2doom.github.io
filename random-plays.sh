06915af1aed8da40e7fb9bc42a499702a2e49dff
Tweet2Doom <tweet2doom@gmail.com>
2021-10-19 19:01:41 +0300

    Do not choose early-game videos of random plays

--- random-plays.sh
+++ random-plays.sh
@@ -10,13 +10,21 @@ cd data/nodes
 
 nodes_all="$(ls)"
 nodes=""
+num_nodes=0
 for i in $nodes_all ; do
-    depth=$(cat ${i}/depth)
+    read -r depth < ${i}/depth
+    read -r frames < ${i}/frames
     if [ $depth -lt 3 ] ; then
         continue
     fi
+    if [ $frames -lt 1050 ] ; then
+        continue
+    fi
     nodes="$nodes $i"
+    num_nodes=$(($num_nodes + 1))
 done
+
+echo "Total nodes: $num_nodes"
 
 nodes_render=$(echo "$nodes" | tr " " "\n" | sort -R | tail -n 10)
 
@@ -35,7 +43,7 @@ for i in $nodes_render ; do
         -output tmp/$i.mp4 \
         -nrecord 350 \
         -framerate 60 \
-        -nfreeze 5 \
+        -nfreeze 10 \
         -render_frame \
         -render_input \
         -render_username || result=$?
