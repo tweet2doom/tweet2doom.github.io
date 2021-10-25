b2bffce01582de4443738333e7b764fe661750c7
Tweet2Doom <tweet2doom@gmail.com>
2021-10-25 09:24:34 +0000

    New states added [Mon 25 Oct 2021 09:24:34 AM UTC]

--- js/graph.js
+++ js/graph.js
@@ -7219,6 +7219,7 @@ nodes.push({ id: '1452504876383195143', label: "tweet2doom", level: 2, group: "n
 nodes.push({ id: '1452505475245907970', label: "tweet2doom", level: 2, group: "node" });
 nodes.push({ id: '1452505715399135236', label: "tweet2doom", level: 2, group: "node" });
 nodes.push({ id: '1452526171330072576', label: "tweet2doom", level: 16, group: "node" });
+nodes.push({ id: '1452566488204955653', label: "tweet2doom", level: 2, group: "node" });
 nodes.push({ id: '1444358348955164684', label: "tweet2doom_info", level: 1, group: "command" });
 nodes.push({ id: '1444359046136479752', label: "ggerganov", level: 1, group: "command" });
 nodes.push({ id: '1444359352073216003', label: "ggerganov", level: 3, group: "command" });
@@ -14437,6 +14438,7 @@ nodes.push({ id: '1452504790617886721', label: "Tur0n_B_g", level: 1, group: "co
 nodes.push({ id: '1452505395142926340', label: "Tur0n_B_g", level: 1, group: "command" });
 nodes.push({ id: '1452505645777829891', label: "mati_x7", level: 1, group: "command" });
 nodes.push({ id: '1452526053587668994', label: "PetrusOelo", level: 15, group: "command" });
+nodes.push({ id: '1452566392838971395', label: "pedrin_gemaplay", level: 1, group: "command" });
 
 edges.push({ from: '1444358348955164684', to: '1444355917160534024' });
 edges.push({ from: '1444358418337239043', to: '1444358348955164684' });
@@ -28874,4 +28876,6 @@ edges.push({ from: '1452505645777829891', to: '1444355917160534024' });
 edges.push({ from: '1452505715399135236', to: '1452505645777829891' });
 edges.push({ from: '1452526053587668994', to: '1451910025006440454' });
 edges.push({ from: '1452526171330072576', to: '1452526053587668994' });
+edges.push({ from: '1452566392838971395', to: '1444355917160534024' });
+edges.push({ from: '1452566488204955653', to: '1452566392838971395' });
 }
