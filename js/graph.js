33e6a533ec85918ef6cb57207ab9baf83b245d01
Tweet2Doom <tweet2doom@gmail.com>
2021-10-27 08:28:48 +0000

    New states added [Wed 27 Oct 2021 08:28:47 AM UTC]

--- js/graph.js
+++ js/graph.js
@@ -7346,6 +7346,8 @@ nodes.push({ id: '1453207660073996288', label: "tweet2doom", level: 4, group: "n
 nodes.push({ id: '1453207810729291777', label: "tweet2doom", level: 2, group: "node" });
 nodes.push({ id: '1453207963615875073', label: "tweet2doom", level: 6, group: "node" });
 nodes.push({ id: '1453257062989942788', label: "tweet2doom", level: 2, group: "node" });
+nodes.push({ id: '1453271968548012033', label: "tweet2doom", level: 2, group: "node" });
+nodes.push({ id: '1453274492462116870', label: "tweet2doom", level: 2, group: "node" });
 nodes.push({ id: '1444358348955164684', label: "tweet2doom_info", level: 1, group: "command" });
 nodes.push({ id: '1444359046136479752', label: "ggerganov", level: 1, group: "command" });
 nodes.push({ id: '1444359352073216003', label: "ggerganov", level: 3, group: "command" });
@@ -14691,6 +14693,8 @@ nodes.push({ id: '1453207532772732932', label: "Poopmuncher15", level: 3, group:
 nodes.push({ id: '1453207637466787840', label: "ELJAV002", level: 1, group: "command" });
 nodes.push({ id: '1453207796590186497', label: "Poopmuncher15", level: 5, group: "command" });
 nodes.push({ id: '1453256941912924169', label: "JaredGviuck", level: 1, group: "command" });
+nodes.push({ id: '1453271855448657920', label: "RincewindTgw", level: 1, group: "command" });
+nodes.push({ id: '1453274379589234689', label: "Maurisioxd_m", level: 1, group: "command" });
 
 edges.push({ from: '1444358348955164684', to: '1444355917160534024' });
 edges.push({ from: '1444358418337239043', to: '1444358348955164684' });
@@ -29382,4 +29386,8 @@ edges.push({ from: '1453207796590186497', to: '1453207660073996288' });
 edges.push({ from: '1453207963615875073', to: '1453207796590186497' });
 edges.push({ from: '1453256941912924169', to: '1444355917160534024' });
 edges.push({ from: '1453257062989942788', to: '1453256941912924169' });
+edges.push({ from: '1453271855448657920', to: '1444355917160534024' });
+edges.push({ from: '1453271968548012033', to: '1453271855448657920' });
+edges.push({ from: '1453274379589234689', to: '1444355917160534024' });
+edges.push({ from: '1453274492462116870', to: '1453274379589234689' });
 }
