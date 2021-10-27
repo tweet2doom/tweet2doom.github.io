53837b02220e004610af7cadbd58ecbe1ab43acb
Tweet2Doom <tweet2doom@gmail.com>
2021-10-27 10:59:36 +0000

    New states added [Wed 27 Oct 2021 10:59:36 AM UTC]

--- js/graph.js
+++ js/graph.js
@@ -7348,6 +7348,7 @@ nodes.push({ id: '1453207963615875073', label: "tweet2doom", level: 6, group: "n
 nodes.push({ id: '1453257062989942788', label: "tweet2doom", level: 2, group: "node" });
 nodes.push({ id: '1453271968548012033', label: "tweet2doom", level: 2, group: "node" });
 nodes.push({ id: '1453274492462116870', label: "tweet2doom", level: 2, group: "node" });
+nodes.push({ id: '1453313718943719427', label: "tweet2doom", level: 2, group: "node" });
 nodes.push({ id: '1444358348955164684', label: "tweet2doom_info", level: 1, group: "command" });
 nodes.push({ id: '1444359046136479752', label: "ggerganov", level: 1, group: "command" });
 nodes.push({ id: '1444359352073216003', label: "ggerganov", level: 3, group: "command" });
@@ -14695,6 +14696,7 @@ nodes.push({ id: '1453207796590186497', label: "Poopmuncher15", level: 5, group:
 nodes.push({ id: '1453256941912924169', label: "JaredGviuck", level: 1, group: "command" });
 nodes.push({ id: '1453271855448657920', label: "RincewindTgw", level: 1, group: "command" });
 nodes.push({ id: '1453274379589234689', label: "Maurisioxd_m", level: 1, group: "command" });
+nodes.push({ id: '1453313643706159106', label: "Izanagi67100923", level: 1, group: "command" });
 
 edges.push({ from: '1444358348955164684', to: '1444355917160534024' });
 edges.push({ from: '1444358418337239043', to: '1444358348955164684' });
@@ -29390,4 +29392,6 @@ edges.push({ from: '1453271855448657920', to: '1444355917160534024' });
 edges.push({ from: '1453271968548012033', to: '1453271855448657920' });
 edges.push({ from: '1453274379589234689', to: '1444355917160534024' });
 edges.push({ from: '1453274492462116870', to: '1453274379589234689' });
+edges.push({ from: '1453313643706159106', to: '1444355917160534024' });
+edges.push({ from: '1453313718943719427', to: '1453313643706159106' });
 }
