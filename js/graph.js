c84a4201ad2366f5ae4318fc7ee73fbc7c1a0505
Tweet2Doom <tweet2doom@gmail.com>
2021-10-28 05:11:37 +0000

    New states added [Thu 28 Oct 2021 05:11:37 AM UTC]

--- js/graph.js
+++ js/graph.js
@@ -7385,6 +7385,7 @@ nodes.push({ id: '1453570926654668803', label: "tweet2doom", level: 2, group: "n
 nodes.push({ id: '1453571070045433858', label: "tweet2doom", level: 2, group: "node" });
 nodes.push({ id: '1453571743822630914', label: "tweet2doom", level: 36, group: "node" });
 nodes.push({ id: '1453576852526833668', label: "tweet2doom", level: 2, group: "node" });
+nodes.push({ id: '1453588638932127750', label: "tweet2doom", level: 2, group: "node" });
 nodes.push({ id: '1444358348955164684', label: "tweet2doom_info", level: 1, group: "command" });
 nodes.push({ id: '1444359046136479752', label: "ggerganov", level: 1, group: "command" });
 nodes.push({ id: '1444359352073216003', label: "ggerganov", level: 3, group: "command" });
@@ -14769,6 +14770,7 @@ nodes.push({ id: '1453570802960457734', label: "Kkevin29033604", level: 1, group
 nodes.push({ id: '1453570975832756229', label: "12Spetnas", level: 1, group: "command" });
 nodes.push({ id: '1453571627543826432', label: "Camilozvi", level: 35, group: "command" });
 nodes.push({ id: '1453576752287174662', label: "Soto71543373", level: 1, group: "command" });
+nodes.push({ id: '1453588562591420416', label: "Noplayer15", level: 1, group: "command" });
 
 edges.push({ from: '1444358348955164684', to: '1444355917160534024' });
 edges.push({ from: '1444358418337239043', to: '1444358348955164684' });
@@ -29538,4 +29540,6 @@ edges.push({ from: '1453571627543826432', to: '1453499189355327489' });
 edges.push({ from: '1453571743822630914', to: '1453571627543826432' });
 edges.push({ from: '1453576752287174662', to: '1444355917160534024' });
 edges.push({ from: '1453576852526833668', to: '1453576752287174662' });
+edges.push({ from: '1453588562591420416', to: '1444355917160534024' });
+edges.push({ from: '1453588638932127750', to: '1453588562591420416' });
 }
