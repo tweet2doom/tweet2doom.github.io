3446ce730070fff3dbb3ea03b39db11d918ccfb5
Tweet2Doom <tweet2doom@gmail.com>
2021-11-02 19:10:53 +0000

    New states added [Tue 02 Nov 2021 07:10:52 PM UTC]

--- js/graph.js
+++ js/graph.js
@@ -7528,6 +7528,12 @@ nodes.push({ id: '1455549102679379968', label: "tweet2doom", level: 2, group: "n
 nodes.push({ id: '1455551565281079305', label: "tweet2doom", level: 2, group: "node" });
 nodes.push({ id: '1455595848180514819', label: "tweet2doom", level: 2, group: "node" });
 nodes.push({ id: '1455603992793436162', label: "tweet2doom", level: 2, group: "node" });
+nodes.push({ id: '1455605541384998913', label: "tweet2doom", level: 258, group: "node" });
+nodes.push({ id: '1455605697798881280', label: "tweet2doom", level: 258, group: "node" });
+nodes.push({ id: '1455605843370651655', label: "tweet2doom", level: 258, group: "node" });
+nodes.push({ id: '1455606238822211588', label: "tweet2doom", level: 2, group: "node" });
+nodes.push({ id: '1455612553284001797', label: "tweet2doom", level: 18, group: "node" });
+nodes.push({ id: '1455612700931895299', label: "tweet2doom", level: 18, group: "node" });
 nodes.push({ id: '1444358348955164684', label: "tweet2doom_info", level: 1, group: "command" });
 nodes.push({ id: '1444359046136479752', label: "ggerganov", level: 1, group: "command" });
 nodes.push({ id: '1444359352073216003', label: "ggerganov", level: 3, group: "command" });
@@ -15055,6 +15061,12 @@ nodes.push({ id: '1455549015643365384', label: "LiquidooscurO", level: 1, group:
 nodes.push({ id: '1455551493097017344', label: "ElasFernndez10", level: 1, group: "command" });
 nodes.push({ id: '1455595758627999757', label: "Christi70665796", level: 1, group: "command" });
 nodes.push({ id: '1455603860567851008', label: "Renesitoramire1", level: 1, group: "command" });
+nodes.push({ id: '1455605350317629442', label: "fadgn2", level: 257, group: "command" });
+nodes.push({ id: '1455605484652728323', label: "fadgn2", level: 257, group: "command" });
+nodes.push({ id: '1455605583910998016', label: "fadgn2", level: 257, group: "command" });
+nodes.push({ id: '1455606153770061841', label: "fadgn2", level: 1, group: "command" });
+nodes.push({ id: '1455612424229433346', label: "Azariel50813191", level: 17, group: "command" });
+nodes.push({ id: '1455612537467281414', label: "Azariel50813191", level: 17, group: "command" });
 
 edges.push({ from: '1444358348955164684', to: '1444355917160534024' });
 edges.push({ from: '1444358418337239043', to: '1444358348955164684' });
@@ -30110,4 +30122,16 @@ edges.push({ from: '1455595758627999757', to: '1444355917160534024' });
 edges.push({ from: '1455595848180514819', to: '1455595758627999757' });
 edges.push({ from: '1455603860567851008', to: '1444355917160534024' });
 edges.push({ from: '1455603992793436162', to: '1455603860567851008' });
+edges.push({ from: '1455605350317629442', to: '1450014343798665223' });
+edges.push({ from: '1455605541384998913', to: '1455605350317629442' });
+edges.push({ from: '1455605484652728323', to: '1450014343798665223' });
+edges.push({ from: '1455605697798881280', to: '1455605484652728323' });
+edges.push({ from: '1455605583910998016', to: '1450014343798665223' });
+edges.push({ from: '1455605843370651655', to: '1455605583910998016' });
+edges.push({ from: '1455606153770061841', to: '1444355917160534024' });
+edges.push({ from: '1455606238822211588', to: '1455606153770061841' });
+edges.push({ from: '1455612424229433346', to: '1447673471295758342' });
+edges.push({ from: '1455612553284001797', to: '1455612424229433346' });
+edges.push({ from: '1455612537467281414', to: '1447673471295758342' });
+edges.push({ from: '1455612700931895299', to: '1455612537467281414' });
 }
