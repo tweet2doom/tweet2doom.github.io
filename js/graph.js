a6e1e4805fc2b72663c8e8d17fb7b73245432faa
Tweet2Doom <tweet2doom@gmail.com>
2021-11-02 07:34:36 +0000

    New states added [Tue 02 Nov 2021 07:34:35 AM UTC]

--- js/graph.js
+++ js/graph.js
@@ -7518,6 +7518,7 @@ nodes.push({ id: '1455377252863053827', label: "tweet2doom", level: 18, group: "
 nodes.push({ id: '1455387437753151491', label: "tweet2doom", level: 2, group: "node" });
 nodes.push({ id: '1455394407927230468', label: "tweet2doom", level: 18, group: "node" });
 nodes.push({ id: '1455415382316040193', label: "tweet2doom", level: 2, group: "node" });
+nodes.push({ id: '1455432490114498563', label: "tweet2doom", level: 18, group: "node" });
 nodes.push({ id: '1444358348955164684', label: "tweet2doom_info", level: 1, group: "command" });
 nodes.push({ id: '1444359046136479752', label: "ggerganov", level: 1, group: "command" });
 nodes.push({ id: '1444359352073216003', label: "ggerganov", level: 3, group: "command" });
@@ -15035,6 +15036,7 @@ nodes.push({ id: '1455377133853986825', label: "diegoalex2525", level: 17, group
 nodes.push({ id: '1455387298204422147', label: "Eljugadormonse_", level: 1, group: "command" });
 nodes.push({ id: '1455394286976049153', label: "EddyFight39", level: 17, group: "command" });
 nodes.push({ id: '1455415258311442434', label: "quiebraselias", level: 1, group: "command" });
+nodes.push({ id: '1455432366936231938', label: "asoslox", level: 17, group: "command" });
 
 edges.push({ from: '1444358348955164684', to: '1444355917160534024' });
 edges.push({ from: '1444358418337239043', to: '1444358348955164684' });
@@ -30070,4 +30072,6 @@ edges.push({ from: '1455394286976049153', to: '1447673471295758342' });
 edges.push({ from: '1455394407927230468', to: '1455394286976049153' });
 edges.push({ from: '1455415258311442434', to: '1444355917160534024' });
 edges.push({ from: '1455415382316040193', to: '1455415258311442434' });
+edges.push({ from: '1455432366936231938', to: '1447673471295758342' });
+edges.push({ from: '1455432490114498563', to: '1455432366936231938' });
 }
