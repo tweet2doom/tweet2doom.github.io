21f46bbf30fd1be7f3a5031179a1f94282fb62da
Tweet2Doom <tweet2doom@gmail.com>
2021-11-02 21:12:21 +0000

    New states added [Tue 02 Nov 2021 09:12:20 PM UTC]

--- js/graph.js
+++ js/graph.js
@@ -7535,6 +7535,7 @@ nodes.push({ id: '1455606238822211588', label: "tweet2doom", level: 2, group: "n
 nodes.push({ id: '1455612553284001797', label: "tweet2doom", level: 18, group: "node" });
 nodes.push({ id: '1455612700931895299', label: "tweet2doom", level: 18, group: "node" });
 nodes.push({ id: '1455622271578648579', label: "tweet2doom", level: 18, group: "node" });
+nodes.push({ id: '1455642332729065479', label: "tweet2doom", level: 18, group: "node" });
 nodes.push({ id: '1444358348955164684', label: "tweet2doom_info", level: 1, group: "command" });
 nodes.push({ id: '1444359046136479752', label: "ggerganov", level: 1, group: "command" });
 nodes.push({ id: '1444359352073216003', label: "ggerganov", level: 3, group: "command" });
@@ -15069,6 +15070,7 @@ nodes.push({ id: '1455606153770061841', label: "fadgn2", level: 1, group: "comma
 nodes.push({ id: '1455612424229433346', label: "Azariel50813191", level: 17, group: "command" });
 nodes.push({ id: '1455612537467281414', label: "Azariel50813191", level: 17, group: "command" });
 nodes.push({ id: '1455622144864440320', label: "MeMelodyP", level: 17, group: "command" });
+nodes.push({ id: '1455642201191555087', label: "jkh1083196", level: 17, group: "command" });
 
 edges.push({ from: '1444358348955164684', to: '1444355917160534024' });
 edges.push({ from: '1444358418337239043', to: '1444358348955164684' });
@@ -30138,4 +30140,6 @@ edges.push({ from: '1455612537467281414', to: '1447673471295758342' });
 edges.push({ from: '1455612700931895299', to: '1455612537467281414' });
 edges.push({ from: '1455622144864440320', to: '1447673471295758342' });
 edges.push({ from: '1455622271578648579', to: '1455622144864440320' });
+edges.push({ from: '1455642201191555087', to: '1447673471295758342' });
+edges.push({ from: '1455642332729065479', to: '1455642201191555087' });
 }
