2e311632df08ca174c6e21d039f25c78de138ee0
Tweet2Doom <tweet2doom@gmail.com>
2021-11-04 15:02:37 +0000

    New states added [Thu 04 Nov 2021 03:02:37 PM UTC]

--- js/graph.js
+++ js/graph.js
@@ -7597,6 +7597,7 @@ nodes.push({ id: '1456234403420639233', label: "tweet2doom", level: 266, group: 
 nodes.push({ id: '1456234812503646212', label: "tweet2doom", level: 266, group: "node" });
 nodes.push({ id: '1456266746000642053', label: "tweet2doom", level: 268, group: "node" });
 nodes.push({ id: '1456267604780142601', label: "tweet2doom", level: 270, group: "node" });
+nodes.push({ id: '1456271333260599305', label: "tweet2doom", level: 272, group: "node" });
 nodes.push({ id: '1444358348955164684', label: "tweet2doom_info", level: 1, group: "command" });
 nodes.push({ id: '1444359046136479752', label: "ggerganov", level: 1, group: "command" });
 nodes.push({ id: '1444359352073216003', label: "ggerganov", level: 3, group: "command" });
@@ -15193,6 +15194,7 @@ nodes.push({ id: '1456234276987539459', label: "migero_usausagi", level: 265, gr
 nodes.push({ id: '1456234681075216399', label: "migero_usausagi", level: 265, group: "command" });
 nodes.push({ id: '1456266598398906391', label: "migero_usausagi", level: 267, group: "command" });
 nodes.push({ id: '1456267470180732941', label: "migero_usausagi", level: 269, group: "command" });
+nodes.push({ id: '1456271193523163151', label: "migero_usausagi", level: 271, group: "command" });
 
 edges.push({ from: '1444358348955164684', to: '1444355917160534024' });
 edges.push({ from: '1444358418337239043', to: '1444358348955164684' });
@@ -30386,4 +30388,6 @@ edges.push({ from: '1456266598398906391', to: '1456234812503646212' });
 edges.push({ from: '1456266746000642053', to: '1456266598398906391' });
 edges.push({ from: '1456267470180732941', to: '1456266746000642053' });
 edges.push({ from: '1456267604780142601', to: '1456267470180732941' });
+edges.push({ from: '1456271193523163151', to: '1456267604780142601' });
+edges.push({ from: '1456271333260599305', to: '1456271193523163151' });
 }
