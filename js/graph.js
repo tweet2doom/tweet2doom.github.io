85e86067c63013393ed11e9aa7341b082cf83612
Tweet2Doom <tweet2doom@gmail.com>
2021-11-04 20:06:17 +0000

    New states added [Thu 04 Nov 2021 08:06:17 PM UTC]

--- js/graph.js
+++ js/graph.js
@@ -7605,6 +7605,7 @@ nodes.push({ id: '1456282578844098567', label: "tweet2doom", level: 6, group: "n
 nodes.push({ id: '1456288167636652039', label: "tweet2doom", level: 40, group: "node" });
 nodes.push({ id: '1456296901305933825', label: "tweet2doom", level: 2, group: "node" });
 nodes.push({ id: '1456332878028099591', label: "tweet2doom", level: 2, group: "node" });
+nodes.push({ id: '1456348266174550019', label: "tweet2doom", level: 2, group: "node" });
 nodes.push({ id: '1444358348955164684', label: "tweet2doom_info", level: 1, group: "command" });
 nodes.push({ id: '1444359046136479752', label: "ggerganov", level: 1, group: "command" });
 nodes.push({ id: '1444359352073216003', label: "ggerganov", level: 3, group: "command" });
@@ -15209,6 +15210,7 @@ nodes.push({ id: '1456282466784841740', label: "demon_03wichol", level: 5, group
 nodes.push({ id: '1456288041673318404', label: "gamer4khd2", level: 39, group: "command" });
 nodes.push({ id: '1456296832561139716', label: "CarreraNuno", level: 1, group: "command" });
 nodes.push({ id: '1456332787468881928', label: "HerbertXiv", level: 1, group: "command" });
+nodes.push({ id: '1456348178840702981', label: "svmtoss", level: 1, group: "command" });
 
 edges.push({ from: '1444358348955164684', to: '1444355917160534024' });
 edges.push({ from: '1444358418337239043', to: '1444358348955164684' });
@@ -30418,4 +30420,6 @@ edges.push({ from: '1456296832561139716', to: '1444355917160534024' });
 edges.push({ from: '1456296901305933825', to: '1456296832561139716' });
 edges.push({ from: '1456332787468881928', to: '1444355917160534024' });
 edges.push({ from: '1456332878028099591', to: '1456332787468881928' });
+edges.push({ from: '1456348178840702981', to: '1444355917160534024' });
+edges.push({ from: '1456348266174550019', to: '1456348178840702981' });
 }
