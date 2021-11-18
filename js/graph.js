91e12040517882cbb51766307ed16680071a2b08
Tweet2Doom <tweet2doom@gmail.com>
2021-11-18 00:50:07 +0000

    New states added [Thu 18 Nov 2021 12:50:07 AM UTC]

--- js/graph.js
+++ js/graph.js
@@ -7776,6 +7776,7 @@ nodes.push({ id: '1461061173621755905', label: "tweet2doom", level: 10, group: "
 nodes.push({ id: '1461061994270834700', label: "tweet2doom", level: 10, group: "node" });
 nodes.push({ id: '1461063449098792962', label: "tweet2doom", level: 10, group: "node" });
 nodes.push({ id: '1461082322443374594', label: "tweet2doom", level: 648, group: "node" });
+nodes.push({ id: '1461130142248583171', label: "tweet2doom", level: 648, group: "node" });
 nodes.push({ id: '1444358348955164684', label: "tweet2doom_info", level: 1, group: "command" });
 nodes.push({ id: '1444359046136479752', label: "ggerganov", level: 1, group: "command" });
 nodes.push({ id: '1444359352073216003', label: "ggerganov", level: 3, group: "command" });
@@ -15551,6 +15552,7 @@ nodes.push({ id: '1461061066331500545', label: "TindroJuegaDoom", level: 9, grou
 nodes.push({ id: '1461061887475564556', label: "TindroJuegaDoom", level: 9, group: "command" });
 nodes.push({ id: '1461063327895937030', label: "TindroJuegaDoom", level: 9, group: "command" });
 nodes.push({ id: '1461082177983193090', label: "Meme2Lucas", level: 647, group: "command" });
+nodes.push({ id: '1461130014548803591', label: "based_gooseguy", level: 647, group: "command" });
 
 edges.push({ from: '1444358348955164684', to: '1444355917160534024' });
 edges.push({ from: '1444358418337239043', to: '1444358348955164684' });
@@ -31102,4 +31104,6 @@ edges.push({ from: '1461063327895937030', to: '1460697269146107908' });
 edges.push({ from: '1461063449098792962', to: '1461063327895937030' });
 edges.push({ from: '1461082177983193090', to: '1451099233352835075' });
 edges.push({ from: '1461082322443374594', to: '1461082177983193090' });
+edges.push({ from: '1461130014548803591', to: '1451099233352835075' });
+edges.push({ from: '1461130142248583171', to: '1461130014548803591' });
 }
