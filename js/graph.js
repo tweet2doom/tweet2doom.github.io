076c92293d3de4b7267a90c5bf8cc2ef78fe4cb0
Tweet2Doom <tweet2doom@gmail.com>
2021-12-01 18:24:56 +0000

    New states added [Wed 01 Dec 2021 06:24:56 PM UTC]

--- js/graph.js
+++ js/graph.js
@@ -7895,6 +7895,7 @@ nodes.push({ id: '1465440573830139915', label: "tweet2doom", level: 240, group: 
 nodes.push({ id: '1465516599595708426', label: "tweet2doom", level: 242, group: "node" });
 nodes.push({ id: '1465517286870163465', label: "tweet2doom", level: 244, group: "node" });
 nodes.push({ id: '1465731153516761095', label: "tweet2doom", level: 2, group: "node" });
+nodes.push({ id: '1466107037239107591', label: "tweet2doom", level: 18, group: "node" });
 nodes.push({ id: '1444358348955164684', label: "tweet2doom_info", level: 1, group: "command" });
 nodes.push({ id: '1444359046136479752', label: "ggerganov", level: 1, group: "command" });
 nodes.push({ id: '1444359352073216003', label: "ggerganov", level: 3, group: "command" });
@@ -15789,6 +15790,7 @@ nodes.push({ id: '1465440438815539208', label: "Octopusso89", level: 239, group:
 nodes.push({ id: '1465516470641831938', label: "Octopusso89", level: 241, group: "command" });
 nodes.push({ id: '1465517158239281158', label: "Octopusso89", level: 243, group: "command" });
 nodes.push({ id: '1465731079185260552', label: "Sindereey", level: 1, group: "command" });
+nodes.push({ id: '1466106909132533765', label: "shinigamydark9", level: 17, group: "command" });
 
 edges.push({ from: '1444358348955164684', to: '1444355917160534024' });
 edges.push({ from: '1444358418337239043', to: '1444358348955164684' });
@@ -31578,4 +31580,6 @@ edges.push({ from: '1465517158239281158', to: '1465516599595708426' });
 edges.push({ from: '1465517286870163465', to: '1465517158239281158' });
 edges.push({ from: '1465731079185260552', to: '1444355917160534024' });
 edges.push({ from: '1465731153516761095', to: '1465731079185260552' });
+edges.push({ from: '1466106909132533765', to: '1445787498899247107' });
+edges.push({ from: '1466107037239107591', to: '1466106909132533765' });
 }
