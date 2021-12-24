d4a09bbcd02eca1ff8937e74f50b1e99665f46c7
Tweet2Doom <tweet2doom@gmail.com>
2021-12-24 19:21:29 +0000

    New states added [Fri 24 Dec 2021 07:21:28 PM UTC]

--- js/graph.js
+++ js/graph.js
@@ -8027,6 +8027,7 @@ nodes.push({ id: '1470407322942033933', label: "tweet2doom", level: 20, group: "
 nodes.push({ id: '1470439274529103878', label: "tweet2doom", level: 20, group: "node" });
 nodes.push({ id: '1473949095345668098', label: "tweet2doom", level: 462, group: "node" });
 nodes.push({ id: '1474090618544660491', label: "tweet2doom", level: 36, group: "node" });
+nodes.push({ id: '1474455615829430286', label: "tweet2doom", level: 2, group: "node" });
 nodes.push({ id: '1444358348955164684', label: "tweet2doom_info", level: 1, group: "command" });
 nodes.push({ id: '1444359046136479752', label: "ggerganov", level: 1, group: "command" });
 nodes.push({ id: '1444359352073216003', label: "ggerganov", level: 3, group: "command" });
@@ -16053,6 +16054,7 @@ nodes.push({ id: '1470407193426112518', label: "Jair0liva", level: 19, group: "c
 nodes.push({ id: '1470439154764890115', label: "RGametest", level: 19, group: "command" });
 nodes.push({ id: '1473948962252013568', label: "devtesting_gg_0", level: 461, group: "command" });
 nodes.push({ id: '1474090482682810370', label: "shokerwz", level: 35, group: "command" });
+nodes.push({ id: '1474455546866532352', label: "zcar00882762", level: 1, group: "command" });
 
 edges.push({ from: '1444358348955164684', to: '1444355917160534024' });
 edges.push({ from: '1444358418337239043', to: '1444358348955164684' });
@@ -32106,4 +32108,6 @@ edges.push({ from: '1473948962252013568', to: '1450375895705919488' });
 edges.push({ from: '1473949095345668098', to: '1473948962252013568' });
 edges.push({ from: '1474090482682810370', to: '1450639996009029638' });
 edges.push({ from: '1474090618544660491', to: '1474090482682810370' });
+edges.push({ from: '1474455546866532352', to: '1444355917160534024' });
+edges.push({ from: '1474455615829430286', to: '1474455546866532352' });
 }
