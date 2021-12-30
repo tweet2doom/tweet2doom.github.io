647bbe35e994d8fb9aa32cba6bb484e1d9138585
Tweet2Doom <tweet2doom@gmail.com>
2021-12-30 18:13:41 +0000

    New states added [Thu 30 Dec 2021 06:13:40 PM UTC]

--- js/graph.js
+++ js/graph.js
@@ -8037,6 +8037,7 @@ nodes.push({ id: '1474888582208884740', label: "tweet2doom", level: 4, group: "n
 nodes.push({ id: '1475342002107658244', label: "tweet2doom", level: 502, group: "node" });
 nodes.push({ id: '1476054505414402051', label: "tweet2doom", level: 2, group: "node" });
 nodes.push({ id: '1476254076354670593', label: "tweet2doom", level: 42, group: "node" });
+nodes.push({ id: '1476615154343231497', label: "tweet2doom", level: 2, group: "node" });
 nodes.push({ id: '1444358348955164684', label: "tweet2doom_info", level: 1, group: "command" });
 nodes.push({ id: '1444359046136479752', label: "ggerganov", level: 1, group: "command" });
 nodes.push({ id: '1444359352073216003', label: "ggerganov", level: 3, group: "command" });
@@ -16073,6 +16074,7 @@ nodes.push({ id: '1474888484997382144', label: "zcar00882762", level: 3, group: 
 nodes.push({ id: '1475341873308848131', label: "sceic", level: 501, group: "command" });
 nodes.push({ id: '1476054404302524417', label: "taku0704vvvw", level: 1, group: "command" });
 nodes.push({ id: '1476253941205803017', label: "juanstxplay", level: 41, group: "command" });
+nodes.push({ id: '1476615058146869269', label: "PauloAntuns", level: 1, group: "command" });
 
 edges.push({ from: '1444358348955164684', to: '1444355917160534024' });
 edges.push({ from: '1444358418337239043', to: '1444358348955164684' });
@@ -32146,4 +32148,6 @@ edges.push({ from: '1476054404302524417', to: '1444355917160534024' });
 edges.push({ from: '1476054505414402051', to: '1476054404302524417' });
 edges.push({ from: '1476253941205803017', to: '1445085083384389643' });
 edges.push({ from: '1476254076354670593', to: '1476253941205803017' });
+edges.push({ from: '1476615058146869269', to: '1444355917160534024' });
+edges.push({ from: '1476615154343231497', to: '1476615058146869269' });
 }
