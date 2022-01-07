efd10004d925e25fc830be4cab4b7eb1eb260836
Tweet2Doom <tweet2doom@gmail.com>
2022-01-07 18:15:10 +0000

    New states added [Fri 07 Jan 2022 06:15:09 PM UTC]

--- js/graph.js
+++ js/graph.js
@@ -8060,6 +8060,7 @@ nodes.push({ id: '1478647122538995715', label: "tweet2doom", level: 2, group: "n
 nodes.push({ id: '1478648300714799107', label: "tweet2doom", level: 2, group: "node" });
 nodes.push({ id: '1478649083879436294', label: "tweet2doom", level: 4, group: "node" });
 nodes.push({ id: '1479406766970523654', label: "tweet2doom", level: 32, group: "node" });
+nodes.push({ id: '1479513855734730757', label: "tweet2doom", level: 100, group: "node" });
 nodes.push({ id: '1444358348955164684', label: "tweet2doom_info", level: 1, group: "command" });
 nodes.push({ id: '1444359046136479752', label: "ggerganov", level: 1, group: "command" });
 nodes.push({ id: '1444359352073216003', label: "ggerganov", level: 3, group: "command" });
@@ -16119,6 +16120,7 @@ nodes.push({ id: '1478647044763836417', label: "julseth_keenan", level: 1, group
 nodes.push({ id: '1478648214421000194', label: "julseth_keenan", level: 1, group: "command" });
 nodes.push({ id: '1478648980204449792', label: "julseth_keenan", level: 3, group: "command" });
 nodes.push({ id: '1479406623370227712', label: "AlvaroSerena", level: 31, group: "command" });
+nodes.push({ id: '1479513715166879747', label: "BenChod14615085", level: 99, group: "command" });
 
 edges.push({ from: '1444358348955164684', to: '1444355917160534024' });
 edges.push({ from: '1444358418337239043', to: '1444358348955164684' });
@@ -32238,4 +32240,6 @@ edges.push({ from: '1478648980204449792', to: '1478648300714799107' });
 edges.push({ from: '1478649083879436294', to: '1478648980204449792' });
 edges.push({ from: '1479406623370227712', to: '1450871792118210567' });
 edges.push({ from: '1479406766970523654', to: '1479406623370227712' });
+edges.push({ from: '1479513715166879747', to: '1446571916471554048' });
+edges.push({ from: '1479513855734730757', to: '1479513715166879747' });
 }
