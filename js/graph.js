d39b711bd356663fa4f179eb49ba2c72e8d36c65
Tweet2Doom <tweet2doom@gmail.com>
2022-01-11 11:00:26 +0000

    New states added [Tue 11 Jan 2022 11:00:25 AM UTC]

--- js/graph.js
+++ js/graph.js
@@ -8068,6 +8068,7 @@ nodes.push({ id: '1480649300682100737', label: "tweet2doom", level: 72, group: "
 nodes.push({ id: '1480689183853273094', label: "tweet2doom", level: 2, group: "node" });
 nodes.push({ id: '1480690100552056834', label: "tweet2doom", level: 4, group: "node" });
 nodes.push({ id: '1480796518441533441', label: "tweet2doom", level: 72, group: "node" });
+nodes.push({ id: '1480850954996133893', label: "tweet2doom", level: 2, group: "node" });
 nodes.push({ id: '1444358348955164684', label: "tweet2doom_info", level: 1, group: "command" });
 nodes.push({ id: '1444359046136479752', label: "ggerganov", level: 1, group: "command" });
 nodes.push({ id: '1444359352073216003', label: "ggerganov", level: 3, group: "command" });
@@ -16135,6 +16136,7 @@ nodes.push({ id: '1480649166447648773', label: "JuanElRoblox1", level: 71, group
 nodes.push({ id: '1480689079545126914', label: "Vortexmind", level: 1, group: "command" });
 nodes.push({ id: '1480689984147496960', label: "Vortexmind", level: 3, group: "command" });
 nodes.push({ id: '1480796405291790336', label: "herobrinesword", level: 71, group: "command" });
+nodes.push({ id: '1480850853464625153', label: "HTOBLOB", level: 1, group: "command" });
 
 edges.push({ from: '1444358348955164684', to: '1444355917160534024' });
 edges.push({ from: '1444358418337239043', to: '1444358348955164684' });
@@ -32270,4 +32272,6 @@ edges.push({ from: '1480689984147496960', to: '1480689183853273094' });
 edges.push({ from: '1480690100552056834', to: '1480689984147496960' });
 edges.push({ from: '1480796405291790336', to: '1452406974931419145' });
 edges.push({ from: '1480796518441533441', to: '1480796405291790336' });
+edges.push({ from: '1480850853464625153', to: '1444355917160534024' });
+edges.push({ from: '1480850954996133893', to: '1480850853464625153' });
 }
