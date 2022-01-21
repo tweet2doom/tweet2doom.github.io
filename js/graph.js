85e32c1be225f24cd31768c125cde702b2098da6
Tweet2Doom <tweet2doom@gmail.com>
2022-01-21 19:14:58 +0000

    New states added [Fri 21 Jan 2022 07:14:57 PM UTC]

--- js/graph.js
+++ js/graph.js
@@ -8105,6 +8105,7 @@ nodes.push({ id: '1482617671183130624', label: "tweet2doom", level: 2, group: "n
 nodes.push({ id: '1482802415933997059', label: "tweet2doom", level: 78, group: "node" });
 nodes.push({ id: '1482802566694060032', label: "tweet2doom", level: 78, group: "node" });
 nodes.push({ id: '1483891013856935937', label: "tweet2doom", level: 2, group: "node" });
+nodes.push({ id: '1484605112295346187', label: "tweet2doom", level: 2, group: "node" });
 nodes.push({ id: '1444358348955164684', label: "tweet2doom_info", level: 1, group: "command" });
 nodes.push({ id: '1444359046136479752', label: "ggerganov", level: 1, group: "command" });
 nodes.push({ id: '1444359352073216003', label: "ggerganov", level: 3, group: "command" });
@@ -16209,6 +16210,7 @@ nodes.push({ id: '1482617585518530560', label: "Leoviathann", level: 1, group: "
 nodes.push({ id: '1482802296903938058', label: "TvBuraczek", level: 77, group: "command" });
 nodes.push({ id: '1482802388427759620', label: "TvBuraczek", level: 77, group: "command" });
 nodes.push({ id: '1483890874148868099', label: "sixneynfuana", level: 1, group: "command" });
+nodes.push({ id: '1484605021929058304', label: "ISkinner1982", level: 1, group: "command" });
 
 edges.push({ from: '1444358348955164684', to: '1444355917160534024' });
 edges.push({ from: '1444358418337239043', to: '1444358348955164684' });
@@ -32418,4 +32420,6 @@ edges.push({ from: '1482802388427759620', to: '1449902259337314308' });
 edges.push({ from: '1482802566694060032', to: '1482802388427759620' });
 edges.push({ from: '1483890874148868099', to: '1444355917160534024' });
 edges.push({ from: '1483891013856935937', to: '1483890874148868099' });
+edges.push({ from: '1484605021929058304', to: '1444355917160534024' });
+edges.push({ from: '1484605112295346187', to: '1484605021929058304' });
 }
