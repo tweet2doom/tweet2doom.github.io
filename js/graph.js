0be0e438a8cdd9ac3d75aa55c230515f1b757377
Tweet2Doom <tweet2doom@gmail.com>
2022-02-27 22:26:36 +0000

    New states added [Sun 27 Feb 2022 10:26:36 PM UTC]

--- js/graph.js
+++ js/graph.js
@@ -8273,6 +8273,7 @@ nodes.push({ id: '1497828454426988546', label: "tweet2doom", level: 646, group: 
 nodes.push({ id: '1497900513500749828', label: "tweet2doom", level: 2, group: "node" });
 nodes.push({ id: '1497974056431374339', label: "tweet2doom", level: 646, group: "node" });
 nodes.push({ id: '1498043933120733187', label: "tweet2doom", level: 2, group: "node" });
+nodes.push({ id: '1498061398273929221', label: "tweet2doom", level: 62, group: "node" });
 nodes.push({ id: '1444358348955164684', label: "tweet2doom_info", level: 1, group: "command" });
 nodes.push({ id: '1444359046136479752', label: "ggerganov", level: 1, group: "command" });
 nodes.push({ id: '1444359352073216003', label: "ggerganov", level: 3, group: "command" });
@@ -16545,6 +16546,7 @@ nodes.push({ id: '1497828249790910464', label: "rdyz9", level: 645, group: "comm
 nodes.push({ id: '1497900395145748483', label: "AjiFakhrian", level: 1, group: "command" });
 nodes.push({ id: '1497973847512928265', label: "waharwi", level: 645, group: "command" });
 nodes.push({ id: '1498043725175406592', label: "Fiiiqqq", level: 1, group: "command" });
+nodes.push({ id: '1498061233655668737', label: "iiceebearr", level: 61, group: "command" });
 
 edges.push({ from: '1444358348955164684', to: '1444355917160534024' });
 edges.push({ from: '1444358418337239043', to: '1444358348955164684' });
@@ -33090,4 +33092,6 @@ edges.push({ from: '1497973847512928265', to: '1451097752067260416' });
 edges.push({ from: '1497974056431374339', to: '1497973847512928265' });
 edges.push({ from: '1498043725175406592', to: '1444355917160534024' });
 edges.push({ from: '1498043933120733187', to: '1498043725175406592' });
+edges.push({ from: '1498061233655668737', to: '1445092506933383169' });
+edges.push({ from: '1498061398273929221', to: '1498061233655668737' });
 }
