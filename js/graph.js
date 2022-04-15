4b79dac1ff5dab655a991de26e85fb9c99e6ac0d
Tweet2Doom <tweet2doom@gmail.com>
2022-04-15 23:22:35 +0000

    New states added [Fri 15 Apr 2022 11:22:35 PM UTC]

--- js/graph.js
+++ js/graph.js
@@ -8354,6 +8354,7 @@ nodes.push({ id: '1515088856755982337', label: "tweet2doom", level: 96, group: "
 nodes.push({ id: '1515089597197344771', label: "tweet2doom", level: 98, group: "node" });
 nodes.push({ id: '1515091397984063490', label: "tweet2doom", level: 100, group: "node" });
 nodes.push({ id: '1515092751934763008', label: "tweet2doom", level: 102, group: "node" });
+nodes.push({ id: '1515100904667684869', label: "tweet2doom", level: 104, group: "node" });
 nodes.push({ id: '1444358348955164684', label: "tweet2doom_info", level: 1, group: "command" });
 nodes.push({ id: '1444359046136479752', label: "ggerganov", level: 1, group: "command" });
 nodes.push({ id: '1444359352073216003', label: "ggerganov", level: 3, group: "command" });
@@ -16707,6 +16708,7 @@ nodes.push({ id: '1515088651071467520', label: "nei0geo", level: 95, group: "com
 nodes.push({ id: '1515089405215748096', label: "nei0geo", level: 97, group: "command" });
 nodes.push({ id: '1515091210234470403', label: "nei0geo", level: 99, group: "command" });
 nodes.push({ id: '1515092519746514957', label: "nei0geo", level: 101, group: "command" });
+nodes.push({ id: '1515100706738421766', label: "nei0geo", level: 103, group: "command" });
 
 edges.push({ from: '1444358348955164684', to: '1444355917160534024' });
 edges.push({ from: '1444358418337239043', to: '1444358348955164684' });
@@ -33414,4 +33416,6 @@ edges.push({ from: '1515091210234470403', to: '1515089597197344771' });
 edges.push({ from: '1515091397984063490', to: '1515091210234470403' });
 edges.push({ from: '1515092519746514957', to: '1515091397984063490' });
 edges.push({ from: '1515092751934763008', to: '1515092519746514957' });
+edges.push({ from: '1515100706738421766', to: '1515092751934763008' });
+edges.push({ from: '1515100904667684869', to: '1515100706738421766' });
 }
