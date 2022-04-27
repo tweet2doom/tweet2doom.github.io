4d7ab0bdea92f6e71b087d586fc1eb5a257b1ee2
Tweet2Doom <tweet2doom@gmail.com>
2022-04-27 17:40:57 +0000

    New states added [Wed 27 Apr 2022 05:40:56 PM UTC]

--- js/graph.js
+++ js/graph.js
@@ -8399,6 +8399,7 @@ nodes.push({ id: '1518579736242499585', label: "tweet2doom", level: 16, group: "
 nodes.push({ id: '1518580818050162688', label: "tweet2doom", level: 16, group: "node" });
 nodes.push({ id: '1518581469517688832', label: "tweet2doom", level: 16, group: "node" });
 nodes.push({ id: '1518581890290233345', label: "tweet2doom", level: 16, group: "node" });
+nodes.push({ id: '1519363784900493312', label: "tweet2doom", level: 2, group: "node" });
 nodes.push({ id: '1444358348955164684', label: "tweet2doom_info", level: 1, group: "command" });
 nodes.push({ id: '1444359046136479752', label: "ggerganov", level: 1, group: "command" });
 nodes.push({ id: '1444359352073216003', label: "ggerganov", level: 3, group: "command" });
@@ -16797,6 +16798,7 @@ nodes.push({ id: '1518579546060210176', label: "KorpinenSaku", level: 15, group:
 nodes.push({ id: '1518580628027056128', label: "KorpinenSaku", level: 15, group: "command" });
 nodes.push({ id: '1518581277372334086', label: "KorpinenSaku", level: 15, group: "command" });
 nodes.push({ id: '1518581684643508225', label: "KorpinenSaku", level: 15, group: "command" });
+nodes.push({ id: '1519363655799824385', label: "abajurur", level: 1, group: "command" });
 
 edges.push({ from: '1444358348955164684', to: '1444355917160534024' });
 edges.push({ from: '1444358418337239043', to: '1444358348955164684' });
@@ -33594,4 +33596,6 @@ edges.push({ from: '1518581277372334086', to: '1518577416175865857' });
 edges.push({ from: '1518581469517688832', to: '1518581277372334086' });
 edges.push({ from: '1518581684643508225', to: '1518577416175865857' });
 edges.push({ from: '1518581890290233345', to: '1518581684643508225' });
+edges.push({ from: '1519363655799824385', to: '1444355917160534024' });
+edges.push({ from: '1519363784900493312', to: '1519363655799824385' });
 }
