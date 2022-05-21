d71b9d9c281deb7cc04a56ead20dc2bd7cb72f17
Tweet2Doom <tweet2doom@gmail.com>
2022-05-21 19:23:23 +0000

    New states added [Sat 21 May 2022 07:23:23 PM UTC]

--- js/graph.js
+++ js/graph.js
@@ -8432,6 +8432,7 @@ nodes.push({ id: '1528057123069997056', label: "tweet2doom", level: 2, group: "n
 nodes.push({ id: '1528057279223824384', label: "tweet2doom", level: 2, group: "node" });
 nodes.push({ id: '1528067416856707074', label: "tweet2doom", level: 2, group: "node" });
 nodes.push({ id: '1528068038838439939', label: "tweet2doom", level: 2, group: "node" });
+nodes.push({ id: '1528086545156153344', label: "tweet2doom", level: 2, group: "node" });
 nodes.push({ id: '1444358348955164684', label: "tweet2doom_info", level: 1, group: "command" });
 nodes.push({ id: '1444359046136479752', label: "ggerganov", level: 1, group: "command" });
 nodes.push({ id: '1444359352073216003', label: "ggerganov", level: 3, group: "command" });
@@ -16863,6 +16864,7 @@ nodes.push({ id: '1528056989208788995', label: "Jugodenaranja31", level: 1, grou
 nodes.push({ id: '1528057144616042498', label: "Jugodenaranja31", level: 1, group: "command" });
 nodes.push({ id: '1528067279010902016', label: "nehunsnchez2", level: 1, group: "command" });
 nodes.push({ id: '1528067859800330242', label: "nehunsnchez2", level: 1, group: "command" });
+nodes.push({ id: '1528086417120841728', label: "QuantumsAlt", level: 1, group: "command" });
 
 edges.push({ from: '1444358348955164684', to: '1444355917160534024' });
 edges.push({ from: '1444358418337239043', to: '1444358348955164684' });
@@ -33726,4 +33728,6 @@ edges.push({ from: '1528067279010902016', to: '1444355917160534024' });
 edges.push({ from: '1528067416856707074', to: '1528067279010902016' });
 edges.push({ from: '1528067859800330242', to: '1444355917160534024' });
 edges.push({ from: '1528068038838439939', to: '1528067859800330242' });
+edges.push({ from: '1528086417120841728', to: '1444355917160534024' });
+edges.push({ from: '1528086545156153344', to: '1528086417120841728' });
 }
