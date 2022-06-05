0ca9c40cd469017096e5dba4cac3b7d445c95772
Tweet2Doom <tweet2doom@gmail.com>
2022-06-05 18:53:46 +0000

    New states added [Sun 05 Jun 2022 06:53:45 PM UTC]

--- js/graph.js
+++ js/graph.js
@@ -8445,6 +8445,7 @@ nodes.push({ id: '1531415892743135233', label: "tweet2doom", level: 386, group: 
 nodes.push({ id: '1532336400024195072', label: "tweet2doom", level: 2, group: "node" });
 nodes.push({ id: '1532929111169150976', label: "tweet2doom", level: 4, group: "node" });
 nodes.push({ id: '1533286323884281857', label: "tweet2doom", level: 2, group: "node" });
+nodes.push({ id: '1533518694202150914', label: "tweet2doom", level: 20, group: "node" });
 nodes.push({ id: '1444358348955164684', label: "tweet2doom_info", level: 1, group: "command" });
 nodes.push({ id: '1444359046136479752', label: "ggerganov", level: 1, group: "command" });
 nodes.push({ id: '1444359352073216003', label: "ggerganov", level: 3, group: "command" });
@@ -16889,6 +16890,7 @@ nodes.push({ id: '1531415697175285763', label: "EdNaitmer", level: 385, group: "
 nodes.push({ id: '1532336290683047937', label: "__alfie_moor__", level: 1, group: "command" });
 nodes.push({ id: '1532928946228310016', label: "KianRetrigods", level: 3, group: "command" });
 nodes.push({ id: '1533286198323687424', label: "XdYono", level: 1, group: "command" });
+nodes.push({ id: '1533518507186569218', label: "YT_BitES", level: 19, group: "command" });
 
 edges.push({ from: '1444358348955164684', to: '1444355917160534024' });
 edges.push({ from: '1444358418337239043', to: '1444358348955164684' });
@@ -33778,4 +33780,6 @@ edges.push({ from: '1532928946228310016', to: '1444359128890085380' });
 edges.push({ from: '1532929111169150976', to: '1532928946228310016' });
 edges.push({ from: '1533286198323687424', to: '1444355917160534024' });
 edges.push({ from: '1533286323884281857', to: '1533286198323687424' });
+edges.push({ from: '1533518507186569218', to: '1445803524110041089' });
+edges.push({ from: '1533518694202150914', to: '1533518507186569218' });
 }
