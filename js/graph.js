2836926bf36de46f9435e6757fd3e5e272983082
Tweet2Doom <tweet2doom@gmail.com>
2022-06-09 11:57:51 +0000

    New states added [Thu 09 Jun 2022 11:57:50 AM UTC]

--- js/graph.js
+++ js/graph.js
@@ -8501,6 +8501,10 @@ nodes.push({ id: '1534524007353139202', label: "tweet2doom", level: 54, group: "
 nodes.push({ id: '1534625719652859905', label: "tweet2doom", level: 30, group: "node" });
 nodes.push({ id: '1534767501233504256', label: "tweet2doom", level: 162, group: "node" });
 nodes.push({ id: '1534769214434721793', label: "tweet2doom", level: 162, group: "node" });
+nodes.push({ id: '1534863028788899840', label: "tweet2doom", level: 2, group: "node" });
+nodes.push({ id: '1534863521284079623', label: "tweet2doom", level: 2, group: "node" });
+nodes.push({ id: '1534863994468585472', label: "tweet2doom", level: 2, group: "node" });
+nodes.push({ id: '1534864486758326273', label: "tweet2doom", level: 2, group: "node" });
 nodes.push({ id: '1444358348955164684', label: "tweet2doom_info", level: 1, group: "command" });
 nodes.push({ id: '1444359046136479752', label: "ggerganov", level: 1, group: "command" });
 nodes.push({ id: '1444359352073216003', label: "ggerganov", level: 3, group: "command" });
@@ -17001,6 +17005,10 @@ nodes.push({ id: '1534523818592686081', label: "Uckerel1", level: 53, group: "co
 nodes.push({ id: '1534625541785010176', label: "Skoorbals", level: 29, group: "command" });
 nodes.push({ id: '1534767319687127040', label: "lad_short", level: 161, group: "command" });
 nodes.push({ id: '1534769035581149184', label: "lad_short", level: 161, group: "command" });
+nodes.push({ id: '1534862916901646336', label: "Le_Berenguer", level: 1, group: "command" });
+nodes.push({ id: '1534863414761234434', label: "Le_Berenguer", level: 1, group: "command" });
+nodes.push({ id: '1534863887002177536', label: "Le_Berenguer", level: 1, group: "command" });
+nodes.push({ id: '1534864381225357313', label: "Le_Berenguer", level: 1, group: "command" });
 
 edges.push({ from: '1444358348955164684', to: '1444355917160534024' });
 edges.push({ from: '1444358418337239043', to: '1444358348955164684' });
@@ -34002,4 +34010,12 @@ edges.push({ from: '1534767319687127040', to: '1467595565441638401' });
 edges.push({ from: '1534767501233504256', to: '1534767319687127040' });
 edges.push({ from: '1534769035581149184', to: '1467595565441638401' });
 edges.push({ from: '1534769214434721793', to: '1534769035581149184' });
+edges.push({ from: '1534862916901646336', to: '1444355917160534024' });
+edges.push({ from: '1534863028788899840', to: '1534862916901646336' });
+edges.push({ from: '1534863414761234434', to: '1444355917160534024' });
+edges.push({ from: '1534863521284079623', to: '1534863414761234434' });
+edges.push({ from: '1534863887002177536', to: '1444355917160534024' });
+edges.push({ from: '1534863994468585472', to: '1534863887002177536' });
+edges.push({ from: '1534864381225357313', to: '1444355917160534024' });
+edges.push({ from: '1534864486758326273', to: '1534864381225357313' });
 }
