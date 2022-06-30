5c895d801c633e0a05f22571b1ff415515df35ea
Tweet2Doom <tweet2doom@gmail.com>
2022-06-30 20:29:58 +0000

    New states added [Thu 30 Jun 2022 08:29:57 PM UTC]

--- js/graph.js
+++ js/graph.js
@@ -8589,6 +8589,7 @@ nodes.push({ id: '1541794076315500545', label: "tweet2doom", level: 2, group: "n
 nodes.push({ id: '1542363180936093696', label: "tweet2doom", level: 2, group: "node" });
 nodes.push({ id: '1542377333373505537', label: "tweet2doom", level: 28, group: "node" });
 nodes.push({ id: '1542558444859068416', label: "tweet2doom", level: 2, group: "node" });
+nodes.push({ id: '1542605095170351104', label: "tweet2doom", level: 2, group: "node" });
 nodes.push({ id: '1444358348955164684', label: "tweet2doom_info", level: 1, group: "command" });
 nodes.push({ id: '1444359046136479752', label: "ggerganov", level: 1, group: "command" });
 nodes.push({ id: '1444359352073216003', label: "ggerganov", level: 3, group: "command" });
@@ -17177,6 +17178,7 @@ nodes.push({ id: '1541793977791385602', label: "3L_computer", level: 1, group: "
 nodes.push({ id: '1542363096865542145', label: "agustin_avilaq", level: 1, group: "command" });
 nodes.push({ id: '1542377173323059200', label: "JackDarkFenix1", level: 27, group: "command" });
 nodes.push({ id: '1542558344803717122', label: "janzenmarkjr", level: 1, group: "command" });
+nodes.push({ id: '1542604907185946627', label: "Le_Berenguer", level: 1, group: "command" });
 
 edges.push({ from: '1444358348955164684', to: '1444355917160534024' });
 edges.push({ from: '1444358418337239043', to: '1444358348955164684' });
@@ -34354,4 +34356,6 @@ edges.push({ from: '1542377173323059200', to: '1445822988671066116' });
 edges.push({ from: '1542377333373505537', to: '1542377173323059200' });
 edges.push({ from: '1542558344803717122', to: '1444355917160534024' });
 edges.push({ from: '1542558444859068416', to: '1542558344803717122' });
+edges.push({ from: '1542604907185946627', to: '1444355917160534024' });
+edges.push({ from: '1542605095170351104', to: '1542604907185946627' });
 }
