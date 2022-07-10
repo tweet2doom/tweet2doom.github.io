f8ad42c94ea219459f65b96974003d2bb849c69e
Tweet2Doom <tweet2doom@gmail.com>
2022-07-10 21:41:00 +0000

    New states added [Sun 10 Jul 2022 09:41:00 PM UTC]

--- js/graph.js
+++ js/graph.js
@@ -8619,6 +8619,7 @@ nodes.push({ id: '1544564855985901573', label: "tweet2doom", level: 2, group: "n
 nodes.push({ id: '1544890904854528001', label: "tweet2doom", level: 2, group: "node" });
 nodes.push({ id: '1545399415619428354', label: "tweet2doom", level: 2, group: "node" });
 nodes.push({ id: '1546224738091122688', label: "tweet2doom", level: 8, group: "node" });
+nodes.push({ id: '1546240538701139972', label: "tweet2doom", level: 2, group: "node" });
 nodes.push({ id: '1444358348955164684', label: "tweet2doom_info", level: 1, group: "command" });
 nodes.push({ id: '1444359046136479752', label: "ggerganov", level: 1, group: "command" });
 nodes.push({ id: '1444359352073216003', label: "ggerganov", level: 3, group: "command" });
@@ -17237,6 +17238,7 @@ nodes.push({ id: '1544564782354771968', label: "BeanlandHenners", level: 1, grou
 nodes.push({ id: '1544890803201667072', label: "OofieChan", level: 1, group: "command" });
 nodes.push({ id: '1545399256466731009', label: "TQ8Ci0qUzjJbpfT", level: 1, group: "command" });
 nodes.push({ id: '1546224571543592960', label: "Domngue12Robert", level: 7, group: "command" });
+nodes.push({ id: '1546240358820069376', label: "dasc17_", level: 1, group: "command" });
 
 edges.push({ from: '1444358348955164684', to: '1444355917160534024' });
 edges.push({ from: '1444358418337239043', to: '1444358348955164684' });
@@ -34474,4 +34476,6 @@ edges.push({ from: '1545399256466731009', to: '1444355917160534024' });
 edges.push({ from: '1545399415619428354', to: '1545399256466731009' });
 edges.push({ from: '1546224571543592960', to: '1449193951169290244' });
 edges.push({ from: '1546224738091122688', to: '1546224571543592960' });
+edges.push({ from: '1546240358820069376', to: '1444355917160534024' });
+edges.push({ from: '1546240538701139972', to: '1546240358820069376' });
 }
