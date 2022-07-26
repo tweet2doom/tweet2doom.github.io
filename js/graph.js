7e3a766b4adb3137104ecb8f08a5306777664066
Tweet2Doom <tweet2doom@gmail.com>
2022-07-26 13:41:48 +0000

    New states added [Tue 26 Jul 2022 01:41:48 PM UTC]

--- js/graph.js
+++ js/graph.js
@@ -8716,6 +8716,7 @@ nodes.push({ id: '1551653757154361349', label: "tweet2doom", level: 770, group: 
 nodes.push({ id: '1551754377387540482', label: "tweet2doom", level: 64, group: "node" });
 nodes.push({ id: '1551835245711867905', label: "tweet2doom", level: 46, group: "node" });
 nodes.push({ id: '1551880784251949056', label: "tweet2doom", level: 2, group: "node" });
+nodes.push({ id: '1551923209351712768', label: "tweet2doom", level: 46, group: "node" });
 nodes.push({ id: '1444358348955164684', label: "tweet2doom_info", level: 1, group: "command" });
 nodes.push({ id: '1444359046136479752', label: "ggerganov", level: 1, group: "command" });
 nodes.push({ id: '1444359352073216003', label: "ggerganov", level: 3, group: "command" });
@@ -17431,6 +17432,7 @@ nodes.push({ id: '1551653602795593728', label: "CentinelaRodri", level: 769, gro
 nodes.push({ id: '1551754196504186880', label: "AufarAditya", level: 63, group: "command" });
 nodes.push({ id: '1551835082763247617', label: "ScarierZ", level: 45, group: "command" });
 nodes.push({ id: '1551880703528386561', label: "Rapido12The", level: 1, group: "command" });
+nodes.push({ id: '1551923045756960771', label: "ananas11221", level: 45, group: "command" });
 
 edges.push({ from: '1444358348955164684', to: '1444355917160534024' });
 edges.push({ from: '1444358418337239043', to: '1444358348955164684' });
@@ -34862,4 +34864,6 @@ edges.push({ from: '1551835082763247617', to: '1450194994170634244' });
 edges.push({ from: '1551835245711867905', to: '1551835082763247617' });
 edges.push({ from: '1551880703528386561', to: '1444355917160534024' });
 edges.push({ from: '1551880784251949056', to: '1551880703528386561' });
+edges.push({ from: '1551923045756960771', to: '1450194994170634244' });
+edges.push({ from: '1551923209351712768', to: '1551923045756960771' });
 }
