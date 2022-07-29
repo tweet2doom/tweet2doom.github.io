130c20fb3b16556788f13dc8ffb79cefe746966a
Tweet2Doom <tweet2doom@gmail.com>
2022-07-29 17:22:24 +0000

    New states added [Fri 29 Jul 2022 05:22:23 PM UTC]

--- js/graph.js
+++ js/graph.js
@@ -8765,6 +8765,7 @@ nodes.push({ id: '1553000171813969924', label: "tweet2doom", level: 32, group: "
 nodes.push({ id: '1553024274176294916', label: "tweet2doom", level: 2, group: "node" });
 nodes.push({ id: '1553052411207491584', label: "tweet2doom", level: 30, group: "node" });
 nodes.push({ id: '1553052841069125633', label: "tweet2doom", level: 30, group: "node" });
+nodes.push({ id: '1553061653339688961', label: "tweet2doom", level: 30, group: "node" });
 nodes.push({ id: '1444358348955164684', label: "tweet2doom_info", level: 1, group: "command" });
 nodes.push({ id: '1444359046136479752', label: "ggerganov", level: 1, group: "command" });
 nodes.push({ id: '1444359352073216003', label: "ggerganov", level: 3, group: "command" });
@@ -17529,6 +17530,7 @@ nodes.push({ id: '1553000006768115712', label: "NotReally1782", level: 31, group
 nodes.push({ id: '1553024099706089472', label: "El_Pinias_", level: 1, group: "command" });
 nodes.push({ id: '1553052239807127558', label: "Luismadrigal315", level: 29, group: "command" });
 nodes.push({ id: '1553052617034563587', label: "david_XD_1", level: 29, group: "command" });
+nodes.push({ id: '1553061488373497856', label: "TailsitoElZorro", level: 29, group: "command" });
 
 edges.push({ from: '1444358348955164684', to: '1444355917160534024' });
 edges.push({ from: '1444358418337239043', to: '1444358348955164684' });
@@ -35058,4 +35060,6 @@ edges.push({ from: '1553052239807127558', to: '1445730197098024962' });
 edges.push({ from: '1553052411207491584', to: '1553052239807127558' });
 edges.push({ from: '1553052617034563587', to: '1445730197098024962' });
 edges.push({ from: '1553052841069125633', to: '1553052617034563587' });
+edges.push({ from: '1553061488373497856', to: '1445730197098024962' });
+edges.push({ from: '1553061653339688961', to: '1553061488373497856' });
 }
