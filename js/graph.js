68fcddf3d59f17d24220eb55c149508c360ed6fa
Tweet2Doom <tweet2doom@gmail.com>
2022-07-29 06:43:34 +0000

    New states added [Fri 29 Jul 2022 06:43:34 AM UTC]

--- js/graph.js
+++ js/graph.js
@@ -8755,6 +8755,7 @@ nodes.push({ id: '1552839350865510400', label: "tweet2doom", level: 2, group: "n
 nodes.push({ id: '1552849562460688385', label: "tweet2doom", level: 2, group: "node" });
 nodes.push({ id: '1552872690511396866', label: "tweet2doom", level: 2, group: "node" });
 nodes.push({ id: '1552882002323554306', label: "tweet2doom", level: 2, group: "node" });
+nodes.push({ id: '1552905994702839808', label: "tweet2doom", level: 2, group: "node" });
 nodes.push({ id: '1444358348955164684', label: "tweet2doom_info", level: 1, group: "command" });
 nodes.push({ id: '1444359046136479752', label: "ggerganov", level: 1, group: "command" });
 nodes.push({ id: '1444359352073216003', label: "ggerganov", level: 3, group: "command" });
@@ -17509,6 +17510,7 @@ nodes.push({ id: '1552839275099602944', label: "MukiKaslana", level: 1, group: "
 nodes.push({ id: '1552849383238033408', label: "Animadx_tom777D", level: 1, group: "command" });
 nodes.push({ id: '1552872591030996992', label: "Xander_anime", level: 1, group: "command" });
 nodes.push({ id: '1552881905254760449', label: "bonnie_ssj", level: 1, group: "command" });
+nodes.push({ id: '1552905825781518336', label: "DiavoloCrims0n", level: 1, group: "command" });
 
 edges.push({ from: '1444358348955164684', to: '1444355917160534024' });
 edges.push({ from: '1444358418337239043', to: '1444358348955164684' });
@@ -35018,4 +35020,6 @@ edges.push({ from: '1552872591030996992', to: '1444355917160534024' });
 edges.push({ from: '1552872690511396866', to: '1552872591030996992' });
 edges.push({ from: '1552881905254760449', to: '1444355917160534024' });
 edges.push({ from: '1552882002323554306', to: '1552881905254760449' });
+edges.push({ from: '1552905825781518336', to: '1444355917160534024' });
+edges.push({ from: '1552905994702839808', to: '1552905825781518336' });
 }
