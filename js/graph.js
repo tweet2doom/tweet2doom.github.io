ff9851a6601fe2c7c7a42d0e4df912e59457cdde
Tweet2Doom <tweet2doom@gmail.com>
2022-07-29 00:37:13 +0000

    New states added [Fri 29 Jul 2022 12:37:13 AM UTC]

--- js/graph.js
+++ js/graph.js
@@ -8744,6 +8744,7 @@ nodes.push({ id: '1552796951002882049', label: "tweet2doom", level: 2, group: "n
 nodes.push({ id: '1552801059202031617', label: "tweet2doom", level: 2, group: "node" });
 nodes.push({ id: '1552803333945135105', label: "tweet2doom", level: 2, group: "node" });
 nodes.push({ id: '1552806625467142146', label: "tweet2doom", level: 2, group: "node" });
+nodes.push({ id: '1552812478949720065', label: "tweet2doom", level: 2, group: "node" });
 nodes.push({ id: '1444358348955164684', label: "tweet2doom_info", level: 1, group: "command" });
 nodes.push({ id: '1444359046136479752', label: "ggerganov", level: 1, group: "command" });
 nodes.push({ id: '1444359352073216003', label: "ggerganov", level: 3, group: "command" });
@@ -17487,6 +17488,7 @@ nodes.push({ id: '1552796849572139009', label: "Foxrite2", level: 1, group: "com
 nodes.push({ id: '1552800992873353216', label: "Meme2Lucas", level: 1, group: "command" });
 nodes.push({ id: '1552803238289842176', label: "LeN746n", level: 1, group: "command" });
 nodes.push({ id: '1552806520852598785', label: "spiderm64814162", level: 1, group: "command" });
+nodes.push({ id: '1552812368165588992', label: "bernabe_cardona", level: 1, group: "command" });
 
 edges.push({ from: '1444358348955164684', to: '1444355917160534024' });
 edges.push({ from: '1444358418337239043', to: '1444358348955164684' });
@@ -34974,4 +34976,6 @@ edges.push({ from: '1552803238289842176', to: '1444355917160534024' });
 edges.push({ from: '1552803333945135105', to: '1552803238289842176' });
 edges.push({ from: '1552806520852598785', to: '1444355917160534024' });
 edges.push({ from: '1552806625467142146', to: '1552806520852598785' });
+edges.push({ from: '1552812368165588992', to: '1444355917160534024' });
+edges.push({ from: '1552812478949720065', to: '1552812368165588992' });
 }
