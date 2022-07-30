8d7a4ac07dd1c4b29b0fdc549f6e849385dd3af5
Tweet2Doom <tweet2doom@gmail.com>
2022-07-30 15:12:00 +0000

    New states added [Sat 30 Jul 2022 03:12:00 PM UTC]

--- js/graph.js
+++ js/graph.js
@@ -8797,6 +8797,7 @@ nodes.push({ id: '1553292427552751616', label: "tweet2doom", level: 4, group: "n
 nodes.push({ id: '1553293048183984129', label: "tweet2doom", level: 2, group: "node" });
 nodes.push({ id: '1553307766969753600', label: "tweet2doom", level: 2, group: "node" });
 nodes.push({ id: '1553314077346242560', label: "tweet2doom", level: 2, group: "node" });
+nodes.push({ id: '1553394671342260226', label: "tweet2doom", level: 2, group: "node" });
 nodes.push({ id: '1444358348955164684', label: "tweet2doom_info", level: 1, group: "command" });
 nodes.push({ id: '1444359046136479752', label: "ggerganov", level: 1, group: "command" });
 nodes.push({ id: '1444359352073216003', label: "ggerganov", level: 3, group: "command" });
@@ -17593,6 +17594,7 @@ nodes.push({ id: '1553292268156596224', label: "XD0CT0RX", level: 3, group: "com
 nodes.push({ id: '1553292880738807809', label: "XD0CT0RX", level: 1, group: "command" });
 nodes.push({ id: '1553307593623408640', label: "iBLOODWARRIOR", level: 1, group: "command" });
 nodes.push({ id: '1553313971838431233', label: "Godmemes2002", level: 1, group: "command" });
+nodes.push({ id: '1553394502261510148', label: "Tusjuegos3G", level: 1, group: "command" });
 
 edges.push({ from: '1444358348955164684', to: '1444355917160534024' });
 edges.push({ from: '1444358418337239043', to: '1444358348955164684' });
@@ -35186,4 +35188,6 @@ edges.push({ from: '1553307593623408640', to: '1444355917160534024' });
 edges.push({ from: '1553307766969753600', to: '1553307593623408640' });
 edges.push({ from: '1553313971838431233', to: '1444355917160534024' });
 edges.push({ from: '1553314077346242560', to: '1553313971838431233' });
+edges.push({ from: '1553394502261510148', to: '1444355917160534024' });
+edges.push({ from: '1553394671342260226', to: '1553394502261510148' });
 }
