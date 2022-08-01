67b22b62d0e071509ee9d9097fda2122b0150cc5
Tweet2Doom <tweet2doom@gmail.com>
2022-08-01 22:07:00 +0000

    New states added [Mon 01 Aug 2022 10:06:59 PM UTC]

--- js/graph.js
+++ js/graph.js
@@ -8818,6 +8818,7 @@ nodes.push({ id: '1554126478937411584', label: "tweet2doom", level: 2, group: "n
 nodes.push({ id: '1554127108259160069', label: "tweet2doom", level: 4, group: "node" });
 nodes.push({ id: '1554128705332682755', label: "tweet2doom", level: 6, group: "node" });
 nodes.push({ id: '1554218009140645888', label: "tweet2doom", level: 134, group: "node" });
+nodes.push({ id: '1554224745423446019', label: "tweet2doom", level: 42, group: "node" });
 nodes.push({ id: '1444358348955164684', label: "tweet2doom_info", level: 1, group: "command" });
 nodes.push({ id: '1444359046136479752', label: "ggerganov", level: 1, group: "command" });
 nodes.push({ id: '1444359352073216003', label: "ggerganov", level: 3, group: "command" });
@@ -17635,6 +17636,7 @@ nodes.push({ id: '1554126406451437570', label: "BlinxRam", level: 1, group: "com
 nodes.push({ id: '1554127025333571584', label: "BlinxRam", level: 3, group: "command" });
 nodes.push({ id: '1554128621815611392', label: "BlinxRam", level: 5, group: "command" });
 nodes.push({ id: '1554217850268577792', label: "Eldoctormenso", level: 133, group: "command" });
+nodes.push({ id: '1554224564942553088', label: "JIngramTx", level: 41, group: "command" });
 
 edges.push({ from: '1444358348955164684', to: '1444355917160534024' });
 edges.push({ from: '1444358418337239043', to: '1444358348955164684' });
@@ -35270,4 +35272,6 @@ edges.push({ from: '1554128621815611392', to: '1554127108259160069' });
 edges.push({ from: '1554128705332682755', to: '1554128621815611392' });
 edges.push({ from: '1554217850268577792', to: '1451047042705416196' });
 edges.push({ from: '1554218009140645888', to: '1554217850268577792' });
+edges.push({ from: '1554224564942553088', to: '1451926784241455104' });
+edges.push({ from: '1554224745423446019', to: '1554224564942553088' });
 }
