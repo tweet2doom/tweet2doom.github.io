62bf4d761fc825ffc85a15cb7ec825f91ae83c42
Tweet2Doom <tweet2doom@gmail.com>
2022-08-01 15:02:36 +0000

    New states added [Mon 01 Aug 2022 03:02:35 PM UTC]

--- js/graph.js
+++ js/graph.js
@@ -8813,6 +8813,7 @@ nodes.push({ id: '1553841527285321729', label: "tweet2doom", level: 2, group: "n
 nodes.push({ id: '1553877149689565192', label: "tweet2doom", level: 24, group: "node" });
 nodes.push({ id: '1553879076913516544', label: "tweet2doom", level: 42, group: "node" });
 nodes.push({ id: '1553989633343717381', label: "tweet2doom", level: 24, group: "node" });
+nodes.push({ id: '1554117807004176385', label: "tweet2doom", level: 2, group: "node" });
 nodes.push({ id: '1444358348955164684', label: "tweet2doom_info", level: 1, group: "command" });
 nodes.push({ id: '1444359046136479752', label: "ggerganov", level: 1, group: "command" });
 nodes.push({ id: '1444359352073216003', label: "ggerganov", level: 3, group: "command" });
@@ -17625,6 +17626,7 @@ nodes.push({ id: '1553841427481874432', label: "RodrigoAlPeres", level: 1, group
 nodes.push({ id: '1553877015006150664', label: "cesargozurreta", level: 23, group: "command" });
 nodes.push({ id: '1553878897741115392', label: "Joan_vegaxd", level: 41, group: "command" });
 nodes.push({ id: '1553989475000254464', label: "MrNobody3330", level: 23, group: "command" });
+nodes.push({ id: '1554117630457516035', label: "Br0nkut", level: 1, group: "command" });
 
 edges.push({ from: '1444358348955164684', to: '1444355917160534024' });
 edges.push({ from: '1444358418337239043', to: '1444358348955164684' });
@@ -35250,4 +35252,6 @@ edges.push({ from: '1553878897741115392', to: '1451926784241455104' });
 edges.push({ from: '1553879076913516544', to: '1553878897741115392' });
 edges.push({ from: '1553989475000254464', to: '1449847401662013445' });
 edges.push({ from: '1553989633343717381', to: '1553989475000254464' });
+edges.push({ from: '1554117630457516035', to: '1444355917160534024' });
+edges.push({ from: '1554117807004176385', to: '1554117630457516035' });
 }
