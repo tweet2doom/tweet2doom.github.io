40adee874117f56ac2b30280fb25690ae24c830e
Tweet2Doom <tweet2doom@gmail.com>
2022-08-06 02:03:00 +0000

    New states added [Sat 06 Aug 2022 02:03:00 AM UTC]

--- js/graph.js
+++ js/graph.js
@@ -8839,6 +8839,7 @@ nodes.push({ id: '1554668025005096963', label: "tweet2doom", level: 2, group: "n
 nodes.push({ id: '1555077997018980352', label: "tweet2doom", level: 770, group: "node" });
 nodes.push({ id: '1555173760445071362', label: "tweet2doom", level: 42, group: "node" });
 nodes.push({ id: '1555339604382961664', label: "tweet2doom", level: 2, group: "node" });
+nodes.push({ id: '1555735220153876480', label: "tweet2doom", level: 770, group: "node" });
 nodes.push({ id: '1444358348955164684', label: "tweet2doom_info", level: 1, group: "command" });
 nodes.push({ id: '1444359046136479752', label: "ggerganov", level: 1, group: "command" });
 nodes.push({ id: '1444359352073216003', label: "ggerganov", level: 3, group: "command" });
@@ -17677,6 +17678,7 @@ nodes.push({ id: '1554667931207815168', label: "Alejand58244996", level: 1, grou
 nodes.push({ id: '1555077844115623936', label: "MateoAGarca1", level: 769, group: "command" });
 nodes.push({ id: '1555173588050796545', label: "PepeElPapaya", level: 41, group: "command" });
 nodes.push({ id: '1555339427924348928', label: "Farinkku_Kamado", level: 1, group: "command" });
+nodes.push({ id: '1555735062850715648', label: "TecroMx", level: 769, group: "command" });
 
 edges.push({ from: '1444358348955164684', to: '1444355917160534024' });
 edges.push({ from: '1444358418337239043', to: '1444358348955164684' });
@@ -35354,4 +35356,6 @@ edges.push({ from: '1555173588050796545', to: '1449426648365944839' });
 edges.push({ from: '1555173760445071362', to: '1555173588050796545' });
 edges.push({ from: '1555339427924348928', to: '1444355917160534024' });
 edges.push({ from: '1555339604382961664', to: '1555339427924348928' });
+edges.push({ from: '1555735062850715648', to: '1451131798235369474' });
+edges.push({ from: '1555735220153876480', to: '1555735062850715648' });
 }
