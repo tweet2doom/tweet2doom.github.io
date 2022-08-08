d59df1556ad1c85061009662e7ebf8733858dc91
Tweet2Doom <tweet2doom@gmail.com>
2022-08-08 02:24:50 +0000

    New states added [Mon 08 Aug 2022 02:24:50 AM UTC]

--- js/graph.js
+++ js/graph.js
@@ -8847,6 +8847,7 @@ nodes.push({ id: '1556302101994668032', label: "tweet2doom", level: 2, group: "n
 nodes.push({ id: '1556303304128446464', label: "tweet2doom", level: 2, group: "node" });
 nodes.push({ id: '1556310110397517828', label: "tweet2doom", level: 678, group: "node" });
 nodes.push({ id: '1556319657140912128', label: "tweet2doom", level: 2, group: "node" });
+nodes.push({ id: '1556461196735307776', label: "tweet2doom", level: 2, group: "node" });
 nodes.push({ id: '1444358348955164684', label: "tweet2doom_info", level: 1, group: "command" });
 nodes.push({ id: '1444359046136479752', label: "ggerganov", level: 1, group: "command" });
 nodes.push({ id: '1444359352073216003', label: "ggerganov", level: 3, group: "command" });
@@ -17693,6 +17694,7 @@ nodes.push({ id: '1556301933618630656', label: "VengadorPoltic1", level: 1, grou
 nodes.push({ id: '1556303127808294913', label: "Lalogarzacommx", level: 1, group: "command" });
 nodes.push({ id: '1556309945074712578', label: "yogi625", level: 677, group: "command" });
 nodes.push({ id: '1556319494741581825', label: "MDakaer", level: 1, group: "command" });
+nodes.push({ id: '1556461090615091200', label: "WillamSlim", level: 1, group: "command" });
 
 edges.push({ from: '1444358348955164684', to: '1444355917160534024' });
 edges.push({ from: '1444358418337239043', to: '1444358348955164684' });
@@ -35386,4 +35388,6 @@ edges.push({ from: '1556309945074712578', to: '1451107341601026048' });
 edges.push({ from: '1556310110397517828', to: '1556309945074712578' });
 edges.push({ from: '1556319494741581825', to: '1444355917160534024' });
 edges.push({ from: '1556319657140912128', to: '1556319494741581825' });
+edges.push({ from: '1556461090615091200', to: '1444355917160534024' });
+edges.push({ from: '1556461196735307776', to: '1556461090615091200' });
 }
