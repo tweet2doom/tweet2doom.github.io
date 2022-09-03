2226c9053358b516f0114179e15f082ba7ad53a1
Tweet2Doom <tweet2doom@gmail.com>
2022-09-03 02:27:52 +0000

    New states added [Sat 03 Sep 2022 02:27:52 AM UTC]

--- js/graph.js
+++ js/graph.js
@@ -8877,6 +8877,7 @@ nodes.push({ id: '1564358889025507330', label: "tweet2doom", level: 26, group: "
 nodes.push({ id: '1565236547573653505', label: "tweet2doom", level: 4, group: "node" });
 nodes.push({ id: '1565242305308672000', label: "tweet2doom", level: 800, group: "node" });
 nodes.push({ id: '1565326809549492225', label: "tweet2doom", level: 2, group: "node" });
+nodes.push({ id: '1565885392678690817', label: "tweet2doom", level: 2, group: "node" });
 nodes.push({ id: '1444358348955164684', label: "tweet2doom_info", level: 1, group: "command" });
 nodes.push({ id: '1444359046136479752', label: "ggerganov", level: 1, group: "command" });
 nodes.push({ id: '1444359352073216003', label: "ggerganov", level: 3, group: "command" });
@@ -17753,6 +17754,7 @@ nodes.push({ id: '1564358702110474240', label: "Ajlaspina", level: 25, group: "c
 nodes.push({ id: '1565236443315945474', label: "devtesting_gg_0", level: 3, group: "command" });
 nodes.push({ id: '1565242131823878144', label: "ggerganov", level: 799, group: "command" });
 nodes.push({ id: '1565326639210299392', label: "SynthNoraa", level: 1, group: "command" });
+nodes.push({ id: '1565885320578576385', label: "SrangeX", level: 1, group: "command" });
 
 edges.push({ from: '1444358348955164684', to: '1444355917160534024' });
 edges.push({ from: '1444358418337239043', to: '1444358348955164684' });
@@ -35506,4 +35508,6 @@ edges.push({ from: '1565242131823878144', to: '1497547951417303044' });
 edges.push({ from: '1565242305308672000', to: '1565242131823878144' });
 edges.push({ from: '1565326639210299392', to: '1444355917160534024' });
 edges.push({ from: '1565326809549492225', to: '1565326639210299392' });
+edges.push({ from: '1565885320578576385', to: '1444355917160534024' });
+edges.push({ from: '1565885392678690817', to: '1565885320578576385' });
 }
