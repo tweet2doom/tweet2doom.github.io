acd16a395f2cea6fb4739b43d614a1ff067ffefb
Tweet2Doom <tweet2doom@gmail.com>
2022-09-23 15:38:34 +0000

    New states added [Fri 23 Sep 2022 03:38:33 PM UTC]

--- js/graph.js
+++ js/graph.js
@@ -8916,6 +8916,7 @@ nodes.push({ id: '1571973853085057024', label: "tweet2doom", level: 2, group: "n
 nodes.push({ id: '1571974001865494528', label: "tweet2doom", level: 2, group: "node" });
 nodes.push({ id: '1572616532773179394', label: "tweet2doom", level: 2, group: "node" });
 nodes.push({ id: '1572997219598336001', label: "tweet2doom", level: 2, group: "node" });
+nodes.push({ id: '1573332574172323840', label: "tweet2doom", level: 2, group: "node" });
 nodes.push({ id: '1444358348955164684', label: "tweet2doom_info", level: 1, group: "command" });
 nodes.push({ id: '1444359046136479752', label: "ggerganov", level: 1, group: "command" });
 nodes.push({ id: '1444359352073216003', label: "ggerganov", level: 3, group: "command" });
@@ -17831,6 +17832,7 @@ nodes.push({ id: '1571973675947261952', label: "wadaw2453", level: 1, group: "co
 nodes.push({ id: '1571973740736684033', label: "wadaw2453", level: 1, group: "command" });
 nodes.push({ id: '1572616433691168768', label: "RandomFandomYT", level: 1, group: "command" });
 nodes.push({ id: '1572997140330283009', label: "UrielPe68277271", level: 1, group: "command" });
+nodes.push({ id: '1573332475044036608', label: "andrivumba", level: 1, group: "command" });
 
 edges.push({ from: '1444358348955164684', to: '1444355917160534024' });
 edges.push({ from: '1444358418337239043', to: '1444358348955164684' });
@@ -35662,4 +35664,6 @@ edges.push({ from: '1572616433691168768', to: '1444355917160534024' });
 edges.push({ from: '1572616532773179394', to: '1572616433691168768' });
 edges.push({ from: '1572997140330283009', to: '1444355917160534024' });
 edges.push({ from: '1572997219598336001', to: '1572997140330283009' });
+edges.push({ from: '1573332475044036608', to: '1444355917160534024' });
+edges.push({ from: '1573332574172323840', to: '1573332475044036608' });
 }
