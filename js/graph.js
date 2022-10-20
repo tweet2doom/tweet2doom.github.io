84ed6c9e53e4bee7847c255e54c1e3e46a6649d8
Tweet2Doom <tweet2doom@gmail.com>
2022-10-20 16:03:36 +0000

    New states added [Thu 20 Oct 2022 04:03:35 PM UTC]

--- js/graph.js
+++ js/graph.js
@@ -8955,6 +8955,7 @@ nodes.push({ id: '1582942599715106821', label: "tweet2doom", level: 14, group: "
 nodes.push({ id: '1582950086048649216', label: "tweet2doom", level: 2, group: "node" });
 nodes.push({ id: '1583069451997454337', label: "tweet2doom", level: 14, group: "node" });
 nodes.push({ id: '1583069611649818626', label: "tweet2doom", level: 14, group: "node" });
+nodes.push({ id: '1583122509654011905', label: "tweet2doom", level: 2, group: "node" });
 nodes.push({ id: '1444358348955164684', label: "tweet2doom_info", level: 1, group: "command" });
 nodes.push({ id: '1444359046136479752', label: "ggerganov", level: 1, group: "command" });
 nodes.push({ id: '1444359352073216003', label: "ggerganov", level: 3, group: "command" });
@@ -17909,6 +17910,7 @@ nodes.push({ id: '1582942425584050176', label: "Anticristoosio1", level: 13, gro
 nodes.push({ id: '1582949990452039681', label: "emuy53_P", level: 1, group: "command" });
 nodes.push({ id: '1583069259055321089', label: "kay_bastan", level: 13, group: "command" });
 nodes.push({ id: '1583069430619070464', label: "kay_bastan", level: 13, group: "command" });
+nodes.push({ id: '1583122438157922304', label: "Rishabh10022005", level: 1, group: "command" });
 
 edges.push({ from: '1444358348955164684', to: '1444355917160534024' });
 edges.push({ from: '1444358418337239043', to: '1444358348955164684' });
@@ -35818,4 +35820,6 @@ edges.push({ from: '1583069259055321089', to: '1449813255397036033' });
 edges.push({ from: '1583069451997454337', to: '1583069259055321089' });
 edges.push({ from: '1583069430619070464', to: '1449813255397036033' });
 edges.push({ from: '1583069611649818626', to: '1583069430619070464' });
+edges.push({ from: '1583122438157922304', to: '1444355917160534024' });
+edges.push({ from: '1583122509654011905', to: '1583122438157922304' });
 }
