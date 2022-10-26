13becc79dc6baeb3fe029f3d34d003d7c33adc54
Tweet2Doom <tweet2doom@gmail.com>
2022-10-26 18:04:39 +0000

    New states added [Wed 26 Oct 2022 06:04:38 PM UTC]

--- js/graph.js
+++ js/graph.js
@@ -8967,6 +8967,7 @@ nodes.push({ id: '1585282174684725252', label: "tweet2doom", level: 2, group: "n
 nodes.push({ id: '1585283069170311171', label: "tweet2doom", level: 4, group: "node" });
 nodes.push({ id: '1585283869393158145', label: "tweet2doom", level: 6, group: "node" });
 nodes.push({ id: '1585285170709643264', label: "tweet2doom", level: 8, group: "node" });
+nodes.push({ id: '1585327636909260800', label: "tweet2doom", level: 156, group: "node" });
 nodes.push({ id: '1444358348955164684', label: "tweet2doom_info", level: 1, group: "command" });
 nodes.push({ id: '1444359046136479752', label: "ggerganov", level: 1, group: "command" });
 nodes.push({ id: '1444359352073216003', label: "ggerganov", level: 3, group: "command" });
@@ -17933,6 +17934,7 @@ nodes.push({ id: '1585282043532959744', label: "Exdetransition1", level: 1, grou
 nodes.push({ id: '1585282933727928321', label: "Exdetransition1", level: 3, group: "command" });
 nodes.push({ id: '1585283718658330624', label: "Exdetransition1", level: 5, group: "command" });
 nodes.push({ id: '1585285007731576832', label: "Exdetransition1", level: 7, group: "command" });
+nodes.push({ id: '1585327437579452416', label: "Pablogamer_054", level: 155, group: "command" });
 
 edges.push({ from: '1444358348955164684', to: '1444355917160534024' });
 edges.push({ from: '1444358418337239043', to: '1444358348955164684' });
@@ -35866,4 +35868,6 @@ edges.push({ from: '1585283718658330624', to: '1585283069170311171' });
 edges.push({ from: '1585283869393158145', to: '1585283718658330624' });
 edges.push({ from: '1585285007731576832', to: '1585283869393158145' });
 edges.push({ from: '1585285170709643264', to: '1585285007731576832' });
+edges.push({ from: '1585327437579452416', to: '1449920475753291776' });
+edges.push({ from: '1585327636909260800', to: '1585327437579452416' });
 }
