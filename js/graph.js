05b6f0a42c1f02e1a8d9c6b212994d479fad6ab0
Tweet2Doom <tweet2doom@gmail.com>
2022-10-29 19:32:53 +0000

    New states added [Sat 29 Oct 2022 07:32:53 PM UTC]

--- js/graph.js
+++ js/graph.js
@@ -8971,6 +8971,7 @@ nodes.push({ id: '1585327636909260800', label: "tweet2doom", level: 156, group: 
 nodes.push({ id: '1585911717980160002', label: "tweet2doom", level: 2, group: "node" });
 nodes.push({ id: '1586206342515232768', label: "tweet2doom", level: 2, group: "node" });
 nodes.push({ id: '1586234051081428994', label: "tweet2doom", level: 2, group: "node" });
+nodes.push({ id: '1586435582800134145', label: "tweet2doom", level: 200, group: "node" });
 nodes.push({ id: '1444358348955164684', label: "tweet2doom_info", level: 1, group: "command" });
 nodes.push({ id: '1444359046136479752', label: "ggerganov", level: 1, group: "command" });
 nodes.push({ id: '1444359352073216003', label: "ggerganov", level: 3, group: "command" });
@@ -17941,6 +17942,7 @@ nodes.push({ id: '1585327437579452416', label: "Pablogamer_054", level: 155, gro
 nodes.push({ id: '1585911589999452160', label: "OctobreAugustin", level: 1, group: "command" });
 nodes.push({ id: '1586206146016079872', label: "lucrebem", level: 1, group: "command" });
 nodes.push({ id: '1586233952024788993', label: "lucrebem", level: 1, group: "command" });
+nodes.push({ id: '1586435405242855424', label: "gaboohgg", level: 199, group: "command" });
 
 edges.push({ from: '1444358348955164684', to: '1444355917160534024' });
 edges.push({ from: '1444358418337239043', to: '1444358348955164684' });
@@ -35882,4 +35884,6 @@ edges.push({ from: '1586206146016079872', to: '1444355917160534024' });
 edges.push({ from: '1586206342515232768', to: '1586206146016079872' });
 edges.push({ from: '1586233952024788993', to: '1444355917160534024' });
 edges.push({ from: '1586234051081428994', to: '1586233952024788993' });
+edges.push({ from: '1586435405242855424', to: '1450153028384960517' });
+edges.push({ from: '1586435582800134145', to: '1586435405242855424' });
 }
