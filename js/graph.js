05b57a26f0700da8e586f1241de40a831d788bd3
Tweet2Doom <tweet2doom@gmail.com>
2022-11-18 15:38:32 +0000

    New states added [Fri 18 Nov 2022 03:38:32 PM UTC]

--- js/graph.js
+++ js/graph.js
@@ -8999,6 +8999,7 @@ nodes.push({ id: '1591195635595845669', label: "tweet2doom", level: 2, group: "n
 nodes.push({ id: '1593467121564061697', label: "tweet2doom", level: 2, group: "node" });
 nodes.push({ id: '1593468337014837248', label: "tweet2doom", level: 4, group: "node" });
 nodes.push({ id: '1593469272268984320', label: "tweet2doom", level: 2, group: "node" });
+nodes.push({ id: '1593628444944109569', label: "tweet2doom", level: 526, group: "node" });
 nodes.push({ id: '1444358348955164684', label: "tweet2doom_info", level: 1, group: "command" });
 nodes.push({ id: '1444359046136479752', label: "ggerganov", level: 1, group: "command" });
 nodes.push({ id: '1444359352073216003', label: "ggerganov", level: 3, group: "command" });
@@ -17997,6 +17998,7 @@ nodes.push({ id: '1591195570152275968', label: "Mob10151617", level: 1, group: "
 nodes.push({ id: '1593467046561456128', label: "Crist12Cc", level: 1, group: "command" });
 nodes.push({ id: '1593468240231120896', label: "Crist12Cc", level: 3, group: "command" });
 nodes.push({ id: '1593469180283584518', label: "Crist12Cc", level: 1, group: "command" });
+nodes.push({ id: '1593628263041175553', label: "devtesting_gg_0", level: 525, group: "command" });
 
 edges.push({ from: '1444358348955164684', to: '1444355917160534024' });
 edges.push({ from: '1444358418337239043', to: '1444358348955164684' });
@@ -35994,4 +35996,6 @@ edges.push({ from: '1593468240231120896', to: '1593467121564061697' });
 edges.push({ from: '1593468337014837248', to: '1593468240231120896' });
 edges.push({ from: '1593469180283584518', to: '1444355917160534024' });
 edges.push({ from: '1593469272268984320', to: '1593469180283584518' });
+edges.push({ from: '1593628263041175553', to: '1497534705104793603' });
+edges.push({ from: '1593628444944109569', to: '1593628263041175553' });
 }
