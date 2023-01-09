9a3fd6e984f907e87e41f70943f1b617acda33f4
Tweet2Doom <tweet2doom@gmail.com>
2023-01-09 23:58:19 +0000

    New states added [Mon 09 Jan 2023 11:58:19 PM UTC]

--- js/graph.js
+++ js/graph.js
@@ -9060,6 +9060,7 @@ nodes.push({ id: '1612399697766793219', label: "tweet2doom", level: 70, group: "
 nodes.push({ id: '1612480115966894082', label: "tweet2doom", level: 70, group: "node" });
 nodes.push({ id: '1612554486089764871', label: "tweet2doom", level: 22, group: "node" });
 nodes.push({ id: '1612575968702484501', label: "tweet2doom", level: 8, group: "node" });
+nodes.push({ id: '1612593361269014529', label: "tweet2doom", level: 2, group: "node" });
 nodes.push({ id: '1444358348955164684', label: "tweet2doom_info", level: 1, group: "command" });
 nodes.push({ id: '1444359046136479752', label: "ggerganov", level: 1, group: "command" });
 nodes.push({ id: '1444359352073216003', label: "ggerganov", level: 3, group: "command" });
@@ -18119,6 +18120,7 @@ nodes.push({ id: '1612399507932684288', label: "Parallas10", level: 69, group: "
 nodes.push({ id: '1612479923440033792', label: "6BrandonLozano", level: 69, group: "command" });
 nodes.push({ id: '1612554310063104000', label: "mi_pin_ga", level: 21, group: "command" });
 nodes.push({ id: '1612575852960841729', label: "robinlopez130", level: 7, group: "command" });
+nodes.push({ id: '1612593278892785667', label: "Nihratak", level: 1, group: "command" });
 
 edges.push({ from: '1444358348955164684', to: '1444355917160534024' });
 edges.push({ from: '1444358418337239043', to: '1444358348955164684' });
@@ -36238,4 +36240,6 @@ edges.push({ from: '1612554310063104000', to: '1452390536602652672' });
 edges.push({ from: '1612554486089764871', to: '1612554310063104000' });
 edges.push({ from: '1612575852960841729', to: '1454422315786194945' });
 edges.push({ from: '1612575968702484501', to: '1612575852960841729' });
+edges.push({ from: '1612593278892785667', to: '1444355917160534024' });
+edges.push({ from: '1612593361269014529', to: '1612593278892785667' });
 }
