ec4440be5967f7628a4e00e47999f6d1206a43ec
Tweet2Doom <tweet2doom@gmail.com>
2023-01-23 19:53:09 +0000

    New states added [Mon 23 Jan 2023 07:53:08 PM UTC]

--- js/graph.js
+++ js/graph.js
@@ -9113,6 +9113,7 @@ nodes.push({ id: '1616511387387236361', label: "tweet2doom", level: 2, group: "n
 nodes.push({ id: '1617325155516137473', label: "tweet2doom", level: 6, group: "node" });
 nodes.push({ id: '1617548086594740226', label: "tweet2doom", level: 48, group: "node" });
 nodes.push({ id: '1617567034375651335', label: "tweet2doom", level: 50, group: "node" });
+nodes.push({ id: '1617608967236079617', label: "tweet2doom", level: 40, group: "node" });
 nodes.push({ id: '1444358348955164684', label: "tweet2doom_info", level: 1, group: "command" });
 nodes.push({ id: '1444359046136479752', label: "ggerganov", level: 1, group: "command" });
 nodes.push({ id: '1444359352073216003', label: "ggerganov", level: 3, group: "command" });
@@ -18225,6 +18226,7 @@ nodes.push({ id: '1616511236425793536', label: "Diver_Luna", level: 1, group: "c
 nodes.push({ id: '1617324962712354817', label: "moylite", level: 5, group: "command" });
 nodes.push({ id: '1617547898325196800', label: "PerlaAlvarezs", level: 47, group: "command" });
 nodes.push({ id: '1617566864695083010', label: "mati_mzon", level: 49, group: "command" });
+nodes.push({ id: '1617608802207174656', label: "walditocardenas", level: 39, group: "command" });
 
 edges.push({ from: '1444358348955164684', to: '1444355917160534024' });
 edges.push({ from: '1444358418337239043', to: '1444358348955164684' });
@@ -36450,4 +36452,6 @@ edges.push({ from: '1617547898325196800', to: '1450335300841099267' });
 edges.push({ from: '1617548086594740226', to: '1617547898325196800' });
 edges.push({ from: '1617566864695083010', to: '1617548086594740226' });
 edges.push({ from: '1617567034375651335', to: '1617566864695083010' });
+edges.push({ from: '1617608802207174656', to: '1450852667685736457' });
+edges.push({ from: '1617608967236079617', to: '1617608802207174656' });
 }
