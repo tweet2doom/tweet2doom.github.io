d9a9e70f4746dacd9f6454fc2b1f5e66e6b554b1
Tweet2Doom <tweet2doom@gmail.com>
2023-01-29 09:38:00 +0000

    New states added [Sun 29 Jan 2023 09:37:59 AM UTC]

--- js/graph.js
+++ js/graph.js
@@ -9136,6 +9136,7 @@ nodes.push({ id: '1619457753365336066', label: "tweet2doom", level: 2, group: "n
 nodes.push({ id: '1619481709065502722', label: "tweet2doom", level: 258, group: "node" });
 nodes.push({ id: '1619536097372938240', label: "tweet2doom", level: 258, group: "node" });
 nodes.push({ id: '1619536389669781506', label: "tweet2doom", level: 258, group: "node" });
+nodes.push({ id: '1619624459417141249', label: "tweet2doom", level: 258, group: "node" });
 nodes.push({ id: '1444358348955164684', label: "tweet2doom_info", level: 1, group: "command" });
 nodes.push({ id: '1444359046136479752', label: "ggerganov", level: 1, group: "command" });
 nodes.push({ id: '1444359352073216003', label: "ggerganov", level: 3, group: "command" });
@@ -18271,6 +18272,7 @@ nodes.push({ id: '1619457645936517120', label: "tomioka_pajero", level: 1, group
 nodes.push({ id: '1619481502626254848', label: "BetaTralk", level: 257, group: "command" });
 nodes.push({ id: '1619535905991331841', label: "LegendsHunter12", level: 257, group: "command" });
 nodes.push({ id: '1619536205473021952', label: "LegendsHunter12", level: 257, group: "command" });
+nodes.push({ id: '1619624259537752064', label: "Sebasti92270799", level: 257, group: "command" });
 
 edges.push({ from: '1444358348955164684', to: '1444355917160534024' });
 edges.push({ from: '1444358418337239043', to: '1444358348955164684' });
@@ -36542,4 +36544,6 @@ edges.push({ from: '1619535905991331841', to: '1456220085933285385' });
 edges.push({ from: '1619536097372938240', to: '1619535905991331841' });
 edges.push({ from: '1619536205473021952', to: '1456220085933285385' });
 edges.push({ from: '1619536389669781506', to: '1619536205473021952' });
+edges.push({ from: '1619624259537752064', to: '1456220085933285385' });
+edges.push({ from: '1619624459417141249', to: '1619624259537752064' });
 }
