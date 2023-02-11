d02d5c4719e4405fb7e86deb96a758bbde3ae748
Tweet2Doom <tweet2doom@gmail.com>
2023-02-11 16:10:53 +0000

    New states added [Sat 11 Feb 2023 04:10:52 PM UTC]

--- js/graph.js
+++ js/graph.js
@@ -9197,6 +9197,7 @@ nodes.push({ id: '1624207085658419201', label: "tweet2doom", level: 2, group: "n
 nodes.push({ id: '1624208452921819140', label: "tweet2doom", level: 2, group: "node" });
 nodes.push({ id: '1624209095031943168', label: "tweet2doom", level: 2, group: "node" });
 nodes.push({ id: '1624425341929328640', label: "tweet2doom", level: 2, group: "node" });
+nodes.push({ id: '1624434230422798336', label: "tweet2doom", level: 2, group: "node" });
 nodes.push({ id: 'node_new', label: "tweet2doom", level: 32, group: "node" });
 nodes.push({ id: '1444358348955164684', label: "tweet2doom_info", level: 1, group: "command" });
 nodes.push({ id: '1444359046136479752', label: "ggerganov", level: 1, group: "command" });
@@ -18400,6 +18401,7 @@ nodes.push({ id: '1624206998517477376', label: "guarenas_miguel", level: 1, grou
 nodes.push({ id: '1624208388463767552', label: "guarenas_miguel", level: 1, group: "command" });
 nodes.push({ id: '1624209016539738113', label: "guarenas_miguel", level: 1, group: "command" });
 nodes.push({ id: '1624425155303710722', label: "Ivanguerreroma5", level: 1, group: "command" });
+nodes.push({ id: '1624434160394805248', label: "guarenas_miguel", level: 1, group: "command" });
 
 edges.push({ from: '1444358348955164684', to: '1444355917160534024' });
 edges.push({ from: '1444358418337239043', to: '1444358348955164684' });
@@ -36805,4 +36807,6 @@ edges.push({ from: '1624209016539738113', to: '1444355917160534024' });
 edges.push({ from: '1624209095031943168', to: '1624209016539738113' });
 edges.push({ from: '1624425155303710722', to: '1444355917160534024' });
 edges.push({ from: '1624425341929328640', to: '1624425155303710722' });
+edges.push({ from: '1624434160394805248', to: '1444355917160534024' });
+edges.push({ from: '1624434230422798336', to: '1624434160394805248' });
 }
