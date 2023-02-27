d4bcb0f54e4647f4f2a231e1077f7b71ad82c52d
Tweet2Doom <tweet2doom@gmail.com>
2023-02-27 01:58:30 +0000

    New states added [Mon 27 Feb 2023 01:58:30 AM UTC]

--- js/graph.js
+++ js/graph.js
@@ -9220,6 +9220,7 @@ nodes.push({ id: '1628632337826160642', label: "tweet2doom", level: 2, group: "n
 nodes.push({ id: '1628998680161157121', label: "tweet2doom", level: 128, group: "node" });
 nodes.push({ id: '1628998829155418113', label: "tweet2doom", level: 128, group: "node" });
 nodes.push({ id: '1629130606968725505', label: "tweet2doom", level: 128, group: "node" });
+nodes.push({ id: '1630024245114707974', label: "tweet2doom", level: 2, group: "node" });
 nodes.push({ id: 'node_new', label: "tweet2doom", level: 32, group: "node" });
 nodes.push({ id: '1444358348955164684', label: "tweet2doom_info", level: 1, group: "command" });
 nodes.push({ id: '1444359046136479752', label: "ggerganov", level: 1, group: "command" });
@@ -18446,6 +18447,7 @@ nodes.push({ id: '1628632215155253250', label: "grecomz", level: 1, group: "comm
 nodes.push({ id: '1628998502436020225', label: "CriminalGran", level: 127, group: "command" });
 nodes.push({ id: '1628998575706218497', label: "CriminalGran", level: 127, group: "command" });
 nodes.push({ id: '1629130441344094209', label: "CesarRodriguti", level: 127, group: "command" });
+nodes.push({ id: '1630024050515877889', label: "AndrewBeez", level: 1, group: "command" });
 
 edges.push({ from: '1444358348955164684', to: '1444355917160534024' });
 edges.push({ from: '1444358418337239043', to: '1444358348955164684' });
@@ -36897,4 +36899,6 @@ edges.push({ from: '1628998575706218497', to: '1449914046636036096' });
 edges.push({ from: '1628998829155418113', to: '1628998575706218497' });
 edges.push({ from: '1629130441344094209', to: '1449914046636036096' });
 edges.push({ from: '1629130606968725505', to: '1629130441344094209' });
+edges.push({ from: '1630024050515877889', to: '1444355917160534024' });
+edges.push({ from: '1630024245114707974', to: '1630024050515877889' });
 }
