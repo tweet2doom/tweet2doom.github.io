795e58423237f0d189f8a0e08f001bdafafc5645
Tweet2Doom <tweet2doom@gmail.com>
2023-03-11 23:32:23 +0000

    New states added [Sat 11 Mar 2023 11:32:22 PM UTC]

--- js/graph.js
+++ js/graph.js
@@ -9228,6 +9228,7 @@ nodes.push({ id: '1631821829709897728', label: "tweet2doom", level: 2, group: "n
 nodes.push({ id: '1632715384833859589', label: "tweet2doom", level: 40, group: "node" });
 nodes.push({ id: '1634267172754030593', label: "tweet2doom", level: 2, group: "node" });
 nodes.push({ id: '1634690761663959040', label: "tweet2doom", level: 2, group: "node" });
+nodes.push({ id: '1634691609030807553', label: "tweet2doom", level: 2, group: "node" });
 nodes.push({ id: 'node_new', label: "tweet2doom", level: 32, group: "node" });
 nodes.push({ id: '1444358348955164684', label: "tweet2doom_info", level: 1, group: "command" });
 nodes.push({ id: '1444359046136479752', label: "ggerganov", level: 1, group: "command" });
@@ -18462,6 +18463,7 @@ nodes.push({ id: '1631821737921748995', label: "sophialoski_", level: 1, group: 
 nodes.push({ id: '1632715184245538818', label: "DiGGiVT", level: 39, group: "command" });
 nodes.push({ id: '1634267088230535169', label: "SebaFleitas10", level: 1, group: "command" });
 nodes.push({ id: '1634690638485618691', label: "Toxic5416", level: 1, group: "command" });
+nodes.push({ id: '1634691423755706369', label: "Toxic5416", level: 1, group: "command" });
 
 edges.push({ from: '1444358348955164684', to: '1444355917160534024' });
 edges.push({ from: '1444358418337239043', to: '1444358348955164684' });
@@ -36929,4 +36931,6 @@ edges.push({ from: '1634267088230535169', to: '1444355917160534024' });
 edges.push({ from: '1634267172754030593', to: '1634267088230535169' });
 edges.push({ from: '1634690638485618691', to: '1444355917160534024' });
 edges.push({ from: '1634690761663959040', to: '1634690638485618691' });
+edges.push({ from: '1634691423755706369', to: '1444355917160534024' });
+edges.push({ from: '1634691609030807553', to: '1634691423755706369' });
 }
