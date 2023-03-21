cc0431ae2efd8f0a6c212cf5620588ee200a44ec
Tweet2Doom <tweet2doom@gmail.com>
2023-03-21 23:22:21 +0000

    New states added [Tue 21 Mar 2023 11:22:21 PM UTC]

--- js/graph.js
+++ js/graph.js
@@ -9242,6 +9242,7 @@ nodes.push({ id: '1635751348149923842', label: "tweet2doom", level: 2, group: "n
 nodes.push({ id: '1636490900388868100', label: "tweet2doom", level: 2, group: "node" });
 nodes.push({ id: '1636504776006410240', label: "tweet2doom", level: 2, group: "node" });
 nodes.push({ id: '1636546471565615106', label: "tweet2doom", level: 56, group: "node" });
+nodes.push({ id: '1638317574910799872', label: "tweet2doom", level: 2, group: "node" });
 nodes.push({ id: 'node_new', label: "tweet2doom", level: 32, group: "node" });
 nodes.push({ id: '1444358348955164684', label: "tweet2doom_info", level: 1, group: "command" });
 nodes.push({ id: '1444359046136479752', label: "ggerganov", level: 1, group: "command" });
@@ -18490,6 +18491,7 @@ nodes.push({ id: '1635751274464395267', label: "_Rodmerj", level: 1, group: "com
 nodes.push({ id: '1636490790288359425', label: "lukitas_16_", level: 1, group: "command" });
 nodes.push({ id: '1636504695085715456', label: "TheYoungBoy4L", level: 1, group: "command" });
 nodes.push({ id: '1636546289746821120', label: "JuanJos51124064", level: 55, group: "command" });
+nodes.push({ id: '1638317497068707840', label: "bar6006", level: 1, group: "command" });
 
 edges.push({ from: '1444358348955164684', to: '1444355917160534024' });
 edges.push({ from: '1444358418337239043', to: '1444358348955164684' });
@@ -36985,4 +36987,6 @@ edges.push({ from: '1636504695085715456', to: '1444355917160534024' });
 edges.push({ from: '1636504776006410240', to: '1636504695085715456' });
 edges.push({ from: '1636546289746821120', to: '1449664043086761984' });
 edges.push({ from: '1636546471565615106', to: '1636546289746821120' });
+edges.push({ from: '1638317497068707840', to: '1444355917160534024' });
+edges.push({ from: '1638317574910799872', to: '1638317497068707840' });
 }
