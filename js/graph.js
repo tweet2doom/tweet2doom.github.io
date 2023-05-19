b56c9d0df00b156944b0d486ea4097f8e3ef3bda
Tweet2Doom <tweet2doom@gmail.com>
2023-05-19 06:31:33 +0000

    New states added [Fri 19 May 2023 06:31:32 AM UTC]

--- js/graph.js
+++ js/graph.js
@@ -9328,6 +9328,7 @@ nodes.push({ id: '1658448241095069696', label: "tweet2doom", level: 2, group: "n
 nodes.push({ id: '1658449633465565186', label: "tweet2doom", level: 4, group: "node" });
 nodes.push({ id: '1658604372618559498', label: "tweet2doom", level: 2, group: "node" });
 nodes.push({ id: '1658891296260382721', label: "tweet2doom", level: 2, group: "node" });
+nodes.push({ id: '1659442347443122176', label: "tweet2doom", level: 2, group: "node" });
 nodes.push({ id: 'node_new', label: "tweet2doom", level: 32, group: "node" });
 nodes.push({ id: '1444358348955164684', label: "tweet2doom_info", level: 1, group: "command" });
 nodes.push({ id: '1444359046136479752', label: "ggerganov", level: 1, group: "command" });
@@ -18662,6 +18663,7 @@ nodes.push({ id: '1658448150493839361', label: "LuizoArt", level: 1, group: "com
 nodes.push({ id: '1658449519623675904', label: "LuizoArt", level: 3, group: "command" });
 nodes.push({ id: '1658604256348237828', label: "fraaaangooo", level: 1, group: "command" });
 nodes.push({ id: '1658891166086053888', label: "ahmetcihanoglu", level: 1, group: "command" });
+nodes.push({ id: '1659442220234047489', label: "coan_henry", level: 1, group: "command" });
 
 edges.push({ from: '1444358348955164684', to: '1444355917160534024' });
 edges.push({ from: '1444358418337239043', to: '1444358348955164684' });
@@ -37329,4 +37331,6 @@ edges.push({ from: '1658604256348237828', to: '1444355917160534024' });
 edges.push({ from: '1658604372618559498', to: '1658604256348237828' });
 edges.push({ from: '1658891166086053888', to: '1444355917160534024' });
 edges.push({ from: '1658891296260382721', to: '1658891166086053888' });
+edges.push({ from: '1659442220234047489', to: '1444355917160534024' });
+edges.push({ from: '1659442347443122176', to: '1659442220234047489' });
 }
